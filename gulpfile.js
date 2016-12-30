105d86d0d0c1abd2c8106c5971d8b49bb8ab97e3
Alan Shortis <alan.shortis@mac.com>
2016-12-30 17:12:45 +0000

    Switched to Yarn, added es and sass linting

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,11 @@ const pkg = require('./package.json'),
       source = require('vinyl-source-stream'),
       del = require('del'),
       gulp = require('gulp'),
+      eslint = require('gulp-eslint'),
       autoprefixer = require('gulp-autoprefixer'),
       sourcemaps = require('gulp-sourcemaps'),
-      sass = require('gulp-sass')
+      sass = require('gulp-sass'),
+      sassLint = require('gulp-sass-lint'),
       header = require('gulp-header'),
       rename = require('gulp-rename'),
       cssnano = require('gulp-cssnano'),
@@ -19,7 +21,7 @@ const pkg = require('./package.json'),
 
 const fileHeader = `/* ${pkg.name} | ${new Date()} */\n`;
 
-const options = {
+const path = {
   "src": {
     "sass": "_src/sass/**/*.scss",
     "js": "_src/js"
@@ -31,6 +33,10 @@ const options = {
   }
 };
 
+function reportError() {
+  console.log('ERROR');
+}
+
 
 gulp.task('serve', ['clean', 'jekyll', 'css', 'js'], () => {
   browserSync.init({
@@ -41,8 +47,8 @@ gulp.task('serve', ['clean', 'jekyll', 'css', 'js'], () => {
       }
     }
   });
-  gulp.watch(options.src.sass, ['css']);
-  gulp.watch(`${options.src.js}/**/*.js`, ['js']);
+  gulp.watch(path.src.sass, ['css']);
+  gulp.watch(`${path.src.js}/**/*.js`, ['js']);
   gulp.watch(['**/*.html', '**/*.md'], ['jekyll']);
 });
 
@@ -53,60 +59,88 @@ gulp.task('jekyll', () => {
 });
 
 
-gulp.task('css', () => {
-  return gulp.src(options.src.sass)
+gulp.task('css', ['sasslint'], () => {
+  return gulp.src(path.src.sass)
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'expanded'})
     .on('error', function(err) {
       sass.logError.call(this, err);
       notifier.notify({
         title: 'Gulp',
-        message: 'SASS error - see terminal for details.'
+        message: 'SASS error'
       });
     }))
     .pipe(autoprefixer({
       browsers: ['last 2 versions']
     }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(options.dest.css))
+    .pipe(gulp.dest(path.dest.css))
     .pipe(browserSync.stream());
 });
 
 
+gulp.task('sasslint', () => {
+  return gulp.src(path.src.sass)
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+    .on('error', () => {
+      notifier.notify({
+        title: 'Gulp',
+        message: 'SASS liniting failed'
+      });
+    });
+});
+
+
 gulp.task('minify', ['css'], () => {
-  return gulp.src(`${options.dest.css}/as.css`)
+  return gulp.src(`${path.dest.css}/as.css`)
     .pipe(rename({
       suffix: '.min'
     }))
     .pipe(cssnano())
     .pipe(header(fileHeader))
-    .pipe(gulp.dest(options.dest.css));
+    .pipe(gulp.dest(path.dest.css));
 });
 
 
-gulp.task('js', () => {
-  return browserify({entries: `${options.src.js}/as.js`, extensions: ['.js'], debug: true})
+gulp.task('js', ['eslint'], () => {
+  return browserify({entries: `${path.src.js}/as.js`, extensions: ['.js'], debug: true})
     .bundle()
     .pipe(source('as.js'))
     .pipe(buffer())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(`${options.dest.js}/`));
+    .pipe(gulp.dest(`${path.dest.js}/`));
+});
+
+
+gulp.task('eslint', () => {
+  return gulp.src(['gulpfile.js', `${path.src.js}/*.js`])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .on('error', () => {
+      notifier.notify({
+        title: 'Gulp',
+        message: 'JS liniting failed'
+      });
+    });
 });
 
 
 gulp.task('uglify', ['js'], () => {
-  return gulp.src([`${options.dest.js}/*.js`, `!${options.dest.js}/**/*.min.js`])
+  return gulp.src([`${path.dest.js}/*.js`, `!${path.dest.js}/**/*.min.js`])
     .pipe(rename({
       suffix: '.min'
     }))
     .pipe(uglify())
     .pipe(header(fileHeader))
-    .pipe(gulp.dest(options.dest.js));
+    .pipe(gulp.dest(path.dest.js));
 });
 
 
 gulp.task('clean', () => {
-  return del.sync([options.dest.site]);
+  return del.sync([path.dest.site]);
 });
 
 
