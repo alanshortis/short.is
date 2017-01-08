1dc8ff358b97ea6ee1801bf3c0fa2e41d86d910c
Alan Shortis <alan.shortis@mac.com>
2017-01-08 18:55:50 +0000

    Added SCSS syntax for Prism

--- _src/js/as.js
+++ _src/js/as.js
@@ -1,4 +1,5 @@
 var analytics = require('./modules/_analytics'),
-    prism = require('prismjs');
+    prism = require('prismjs'),
+    prismScss = require('./modules/_prism-scss');
 
 analytics.goog();
