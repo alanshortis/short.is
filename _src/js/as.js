05b28bf79723f69e3acc11b2e67cc551beb1b9c3
Alan Shortis <alan.shortis@mac.com>
2017-01-09 14:40:54 +0000

    Conditionally load JS modules

--- _src/js/as.js
+++ _src/js/as.js
@@ -1,5 +1,8 @@
-var analytics = require('./modules/_analytics'),
-    prism = require('prismjs'),
-    prismScss = require('./modules/_prism-scss');
+var analytics = require('./modules/_analytics');
+
+if (document.body.className.match('post')) {
+  var prism = require('prismjs');
+  var prismScss = require('./modules/_prism-scss');
+}
 
 analytics.goog();
