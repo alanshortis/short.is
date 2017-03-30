ed1bc545ee971ed8db902815a7df8492491ec23e
Alan Shortis <alan.shortis@mac.com>
2017-03-30 18:18:26 +0100

    Directly require prism js

--- _src/js/as.js
+++ _src/js/as.js
@@ -1,8 +1,8 @@
 var analytics = require('./modules/_analytics');
 
 if (document.body.className.match('post')) {
-  var prism = require('prismjs');
-  var prismScss = require('./modules/_prism-scss');
+  require('prismjs');
+  require('./modules/_prism-scss');
 }
 
 analytics.goog();
