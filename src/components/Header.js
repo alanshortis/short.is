f9e16a1f01b8e52801a3f8565720e3f38c8d833a
alanshortis <alan.shortis@mac.com>
2019-03-17 20:45:34 +0000

    Add styled components and stylelint

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const Header = ({ siteTitle }) => <header>{siteTitle}</header>;
+const StyledHeader = styled.header`
+  color: red;
+`;
+
+const Header = ({ siteTitle }) => <StyledHeader>{siteTitle}</StyledHeader>;
 
 Header.propTypes = {
   siteTitle: PropTypes.string.isRequired,
