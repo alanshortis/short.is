29ebea93f7bcd7ae7962c97dcd47260c90011633
Alan Shortis <alan.shortis@experian.com>
2019-03-30 17:55:21 +0000

    Use images from Spotify CDN

--- src/pages/playlists/components/Playlist.jsx
+++ src/pages/playlists/components/Playlist.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Playlist = ({ node }) => (
-  <a key={node.spotifyId} href={node.external_urls.spotify}>
-    <h2>{node.name}</h2>
-    <h3>{node.tracks.total}</h3>
-    {/* <Img fluid={image.localFile.childImageSharp.fluid} alt="" /> */}
-    <img src={node.image.localFile.childImageSharp.fluid.src} alt="" />
-  </a>
-);
+const Playlist = ({ node }) => {
+  const { spotifyId, name, tracks, images, external_urls: urls } = node;
+  return (
+    <a key={spotifyId} href={urls.spotify}>
+      <h2>{name}</h2>
+      <h3>{tracks.total}</h3>
+      <img src={images[1].url} srcSet={`${images[0].url} 2x`} alt="" />
+    </a>
+  );
+};
 
 Playlist.propTypes = {
   node: PropTypes.shape({
