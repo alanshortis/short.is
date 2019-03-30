14e7c06d0a576e030f6be52ba5081b6192825546
Alan Shortis <alan.shortis@experian.com>
2019-03-30 14:37:43 +0000

    Split playlists

--- src/components/PlaylistList.jsx
+++ src/components/PlaylistList.jsx
@@ -4,7 +4,36 @@ import { StaticQuery, graphql } from 'gatsby';
 
 const playlistsQuery = graphql`
   {
-    allSpotifyPlaylist {
+    playlists: allSpotifyPlaylist(
+      sort: { fields: [order], order: ASC }
+      filter: { name: { regex: "/^((?![0-9]{4}).)*$/" } }
+    ) {
+      edges {
+        node {
+          spotifyId
+          name
+          external_urls {
+            spotify
+          }
+          image {
+            localFile {
+              childImageSharp {
+                fluid(maxWidth: 400) {
+                  src
+                }
+              }
+            }
+          }
+          tracks {
+            total
+          }
+        }
+      }
+    }
+    annual: allSpotifyPlaylist(
+      sort: { fields: [order], order: ASC }
+      filter: { name: { regex: "/[0-9]{4}/" } }
+    ) {
       edges {
         node {
           spotifyId
@@ -33,20 +62,39 @@ const playlistsQuery = graphql`
 const PlaylistList = () => (
   <StaticQuery
     query={playlistsQuery}
-    render={({ allSpotifyPlaylist }) =>
-      allSpotifyPlaylist.edges.map(({ node }) => {
-        const { spotifyId, name, image } = node;
-        const { total } = node.tracks;
-        return (
-          <a key={spotifyId} href={node.external_urls.spotify}>
-            <h2>{name}</h2>
-            <h3>{total}</h3>
-            {/* <Img fluid={image.localFile.childImageSharp.fluid} alt="" /> */}
-            <img src={image.localFile.childImageSharp.fluid.src} alt="" />
-          </a>
-        );
-      })
-    }
+    render={({ playlists, annual }) => {
+      return (
+        <>
+          <h2>Playlists</h2>
+          {playlists.edges.map(({ node }) => {
+            const { spotifyId, name, image } = node;
+            const { total } = node.tracks;
+            return (
+              <a key={spotifyId} href={node.external_urls.spotify}>
+                <h2>{name}</h2>
+                <h3>{total}</h3>
+                {/* <Img fluid={image.localFile.childImageSharp.fluid} alt="" /> */}
+                <img src={image.localFile.childImageSharp.fluid.src} alt="" />
+              </a>
+            );
+          })}
+
+          <h2>Annual</h2>
+          {annual.edges.map(({ node }) => {
+            const { spotifyId, name, image } = node;
+            const { total } = node.tracks;
+            return (
+              <a key={spotifyId} href={node.external_urls.spotify}>
+                <h2>{name}</h2>
+                <h3>{total}</h3>
+                {/* <Img fluid={image.localFile.childImageSharp.fluid} alt="" /> */}
+                <img src={image.localFile.childImageSharp.fluid.src} alt="" />
+              </a>
+            );
+          })}
+        </>
+      );
+    }}
   />
 );
 
