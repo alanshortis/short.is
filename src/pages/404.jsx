5ba50c802b6f2efcb0a0d3e9ddda511f9c137edb
Alan Shortis <alan.shortis@experian.com>
2019-06-05 11:03:42 +0100

    Rename pages

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 
-const NotFoundPage = () => (
+const NotFound = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>404</h1>
   </Layout>
 );
 
-export default NotFoundPage;
+export default NotFound;
