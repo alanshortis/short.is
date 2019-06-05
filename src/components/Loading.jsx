123cdd9b9c5187b19d63e6057b092052e47e4c07
Alan Shortis <alan.shortis@experian.com>
2019-06-05 11:50:54 +0100

    Loading spinner with temp design

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
-const Loading = () => <div>Loading...</div>;
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Loading = styled.div`
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+  background-color: transparent;
+  border: 5px solid #ccc;
+  border-top-color: #666;
+  animation: ${rotate} 1s linear infinite;
+`;
 
 export default Loading;
