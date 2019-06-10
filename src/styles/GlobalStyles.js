3e04fce404abf77f523b043b04c4087b64bd74a8
Alan Shortis <alan.shortis@experian.com>
2019-06-10 15:04:53 +0100

    Use fonts

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,7 +1,40 @@
 import { createGlobalStyle } from 'styled-components';
 import { normalize } from 'polished';
+import fonts from './fonts';
 
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: ${fonts.settings.face};
+    font-weight: ${fonts.settings.weight};
+    font-style: normal;
+    font-display: swap;
+    src: local(${fonts.settings.face}), url(${fonts.sans}) format('woff2');
+  }
+
+  @font-face {
+    font-family: ${fonts.settings.face};
+    font-weight: ${fonts.settings.weight};
+    font-style: italic;
+    font-display: swap;
+    src: local(${fonts.settings.face}), url(${fonts.sansItalic}) format('woff2');
+  }
+
+  @font-face {
+    font-family: ${fonts.settings.face};
+    font-weight: ${fonts.settings.weightBold};
+    font-style: normal;
+    font-display: swap;
+    src: local(${fonts.settings.face}), url(${fonts.sansBold}) format('woff2');
+  }
+
+  @font-face {
+    font-family: ${fonts.settings.face};
+    font-weight: ${fonts.settings.weightBold};
+    font-style: italic;
+    font-display: swap;
+    src: local(${fonts.settings.face}), url(${fonts.sansBoldItalic}) format('woff2');
+  }
+
   ${normalize()}
 
   html {
@@ -12,6 +45,12 @@ const GlobalStyles = createGlobalStyle`
     min-height: 100%;
     background-color: ${p => p.theme.colors.background};
     color: ${p => p.theme.colors.type};
+    font-family: ${fonts.settings.face}, sans-serif;
+    font-weight: ${fonts.settings.weight};
+  }
+
+  strong, b {
+    font-weight: ${fonts.settings.weightBold};
   }
 `;
 
--- src/styles/PrismStyles.js
+++ src/styles/PrismStyles.js
@@ -1,6 +1,27 @@
 import { createGlobalStyle } from 'styled-components';
+import fonts from './fonts';
 
 const PrismStyles = createGlobalStyle`
+  @font-face {
+    font-family: ${fonts.settings.faceMono};
+    font-weight: ${fonts.settings.weight};
+    font-style: normal;
+    font-display: swap;
+    src: local(${fonts.settings.faceMono}), url(${fonts.mono}) format('woff2');
+  }
+
+  @font-face {
+    font-family: ${fonts.settings.faceMono};
+    font-weight: ${fonts.settings.weight};
+    font-style: italic;
+    font-display: swap;
+    src: local(${fonts.settings.faceMono}), url(${fonts.monoItalic}) format('woff2');
+  }
+
+  code {
+    font-family: ${fonts.settings.faceMono};
+  }
+
   .gatsby-highlight-code-line {
     display: block;
     background-color: ${p => p.theme.colors.accent};
