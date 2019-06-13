b4e17b384f2fddaa8bac2b17e46240525998ad9b
Alan Shortis <alan.shortis@experian.com>
2019-06-13 21:27:57 +0100

    Refactor font imports

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,56 +1,26 @@
 import { createGlobalStyle } from 'styled-components';
 import { normalize } from 'polished';
-import fonts from './fonts';
+import atRules from './fonts';
 
 const GlobalStyles = createGlobalStyle`
-  @font-face {
-    font-family: ${fonts.settings.face};
-    font-weight: ${fonts.settings.weight};
-    font-style: normal;
-    font-display: swap;
-    src: local(${fonts.settings.face}), url(${fonts.sans}) format('woff2');
-  }
-
-  @font-face {
-    font-family: ${fonts.settings.face};
-    font-weight: ${fonts.settings.weight};
-    font-style: italic;
-    font-display: swap;
-    src: local(${fonts.settings.face}), url(${fonts.sansItalic}) format('woff2');
-  }
-
-  @font-face {
-    font-family: ${fonts.settings.face};
-    font-weight: ${fonts.settings.weightBold};
-    font-style: normal;
-    font-display: swap;
-    src: local(${fonts.settings.face}), url(${fonts.sansBold}) format('woff2');
-  }
-
-  @font-face {
-    font-family: ${fonts.settings.face};
-    font-weight: ${fonts.settings.weightBold};
-    font-style: italic;
-    font-display: swap;
-    src: local(${fonts.settings.face}), url(${fonts.sansBoldItalic}) format('woff2');
-  }
-
-  ${normalize()}
+  ${atRules};
+  ${normalize()};
 
   html {
     height: 100%;
   }
 
   body {
+    background-color: ${p => p.theme.color.background};
+    color: ${p => p.theme.color.type};
+    font-family: ${p => p.theme.font.face}, sans-serif;
+    font-size: ${p => p.theme.font.baseSize};
+    font-weight: ${p => p.theme.font.weight};
     min-height: 100%;
-    background-color: ${p => p.theme.colors.background};
-    color: ${p => p.theme.colors.type};
-    font-family: ${fonts.settings.face}, sans-serif;
-    font-weight: ${fonts.settings.weight};
   }
 
   strong, b {
-    font-weight: ${fonts.settings.weightBold};
+    font-weight: ${p => p.theme.font.weightBold};
   }
 
   em, i {
