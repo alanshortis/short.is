f950092873006b7cc525c2b4aa030e66deed881e
Alan Shortis <alanshortis@Alans-MacBook-Pro.local>
2019-07-10 19:42:46 +0100

    Add favicon and theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const theme = require('./src/styles/theme.json');
 
 module.exports = {
   siteMetadata: {
@@ -61,18 +62,18 @@ module.exports = {
         },
       },
     },
-    // {
-    //   resolve: 'gatsby-plugin-manifest',
-    //   options: {
-    //     name: 'Alan Shortis',
-    //     short_name: 'short.is',
-    //     start_url: '/',
-    //     background_color: '#663399',
-    //     theme_color: '#663399',
-    //     display: 'minimal-ui',
-    //     icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Alan Shortis',
+        short_name: 'short.is',
+        start_url: '/',
+        background_color: theme.color.backgroundDark,
+        theme_color: theme.color.accent,
+        display: 'standalone',
+        icon: 'src/img/favicon.png',
+      },
+    },
     // 'gatsby-plugin-offline',
   ],
 };
