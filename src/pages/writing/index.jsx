9082c1cbeb9251837e0ea2418ce3932e2ad2b02a
Alan Shortis <alanshortis@Alans-MacBook-Pro.local>
2019-07-10 19:58:36 +0100

    Container for post list

--- src/pages/writing/index.jsx
+++ src/pages/writing/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 import Layout from '../../components/Layout';
 import SEO from '../../components/SEO';
 import List from './components/List';
 
+const Container = styled.div`
+  width: 100%;
+  max-width: 60rem;
+  padding: 0 2rem;
+  margin: 0 auto;
+  margin-top: 5rem;
+`;
+
 const Writing = () => (
   <Layout>
     <SEO title="Writing" />
-    <h1>Writing</h1>
-    <List />
+    <Container>
+      <List />
+    </Container>
   </Layout>
 );
 
