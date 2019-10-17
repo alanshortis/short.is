a40e44cf4051a830d5de222593d4ffa80f5b63ed
Alan Shortis <alanshortis@Alans-MacBook-Pro.local>
2019-10-17 17:10:25 +0100

    Index for components

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { default as Layout } from './Layout';
 export { default as Meta } from './Meta';
+export { default as Header } from './Header';
+export { default as Menu } from './Menu';
+export { default as Icon } from './Icon';
