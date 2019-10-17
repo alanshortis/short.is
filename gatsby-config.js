ddb51af5ec8f62d1f11c1b2a5b0e978f693b2cc3
Alan Shortis <alanshortis@Alans-MacBook-Pro.local>
2019-10-17 15:03:58 +0100

    Favicon support

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,4 +4,12 @@ module.exports = {
     description: 'Alan Shortis is a web developer',
     title: 'Alan Shortis',
   },
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './src/img/favicon.png',
+      },
+    },
+  ],
 };
