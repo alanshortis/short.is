38c8ebe3625ce6fd5f59170ce0cf255cda91519d
Alan Shortis <alanshortis@Alans-MacBook-Pro.local>
2019-10-27 22:35:49 +0000

    Change fount source

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,8 +1,8 @@
 import { css } from 'styled-components';
-import regular from '../../node_modules/inter-ui/Inter (web)/Inter-Regular.woff2';
-import regularItalic from '../../node_modules/inter-ui/Inter (web)/Inter-Italic.woff2';
-import bold from '../../node_modules/inter-ui/Inter (web)/Inter-Bold.woff2';
-import boldItalic from '../../node_modules/inter-ui/Inter (web)/Inter-BoldItalic.woff2';
+import regular from '../../node_modules/typeface-inter/Inter (web)/Inter-Regular.woff2';
+import regularItalic from '../../node_modules/typeface-inter/Inter (web)/Inter-Italic.woff2';
+import bold from '../../node_modules/typeface-inter/Inter (web)/Inter-Bold.woff2';
+import boldItalic from '../../node_modules/typeface-inter/Inter (web)/Inter-BoldItalic.woff2';
 
 const fontImports = css`
   @font-face {
