638e68703866d1d7fc0d02b8287442ed0b04f113
Alan Shortis <alanshortis@Alans-MacBook-Pro.local>
2019-11-17 19:19:47 +0000

    Page transitions

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
 import GlobalStyles from '../styles/Global';
-import { Header } from '.';
+import { Header, SlideIn } from '.';
 
 const Layout = ({ children, isDark }) => (
   <ThemeProvider theme={theme}>
     <>
       <GlobalStyles isDark={isDark} />
       <Header isDark={isDark} />
-      {children}
+      <SlideIn as="main">{children}</SlideIn>
     </>
   </ThemeProvider>
 );
--- src/components/index.js
+++ src/components/index.js
@@ -6,4 +6,5 @@ export { default as Layout } from './Layout';
 export { default as Menu } from './Menu';
 export { default as Meta } from './Meta';
 export { default as NextPost } from './NextPost';
+export { default as SlideIn } from './SlideIn';
 export { default as Time } from './Time';
