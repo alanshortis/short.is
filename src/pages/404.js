ac1728336e46a4d3eb8699342e5e239c1add2f0d
Alan Shortis <alanshortis@Alans-MacBook-Pro.local>
2019-11-18 16:45:25 +0000

    Quick 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Layout, Meta } from '../components';
+
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - ${p => p.theme.headerHeight});
+  justify-content: center;
+  margin: 0 auto;
+  max-width: ${p => p.theme.container};
+  padding: ${p => p.theme.contentMargin};
+  width: 100%;
+`;
 
 const NotFound = () => (
   <>
     <Meta title="404" />
-    <Layout isDark>404</Layout>
+    <Layout isDark>
+      <Container>
+        <div>
+          <h1>404</h1>
+          <p>Whatever you&rsquo;re looking for isn&rsquo;t here.</p>
+        </div>
+      </Container>
+    </Layout>
   </>
 );
 
