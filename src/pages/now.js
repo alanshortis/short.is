55d860bfb505b6496b472ad8a7014638a9033ed2
Alan Shortis <alanshortis@Alans-MacBook-Pro.local>
2020-01-19 20:36:43 +0000

    Update Now!

--- src/pages/now.js
+++ src/pages/now.js
@@ -1,60 +1,60 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Layout, Time, Meta } from '../components';
 import {
   StyledPost,
   PostContainer,
   PostTitle,
   PostIntro,
-  CodeFont,
 } from '../components/writing/PostPage.style';
+
+const A = ({ href, children }) => (
+  <a href={href} target="_blank" rel="noopener noreferrer">
+    {children}
+  </a>
+);
+
+A.propTypes = {
+  href: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
 const Now = () => (
   <>
     <Meta title="Now" description="What I'm working on right now." />
     <Layout>
       <PostContainer>
-        <CodeFont />
-        <Time date="2020-01-02" pre="Updated" />
+        <Time date="2020-01-19" pre="Updated" />
         <PostTitle>Now</PostTitle>
         <PostIntro>Work, goals and tasks currently on my plate.</PostIntro>
         <StyledPost>
           <ul>
             <li>
               Training for{' '}
-              <a
-                href="https://lepapemarmottegranfondoalpes.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Marmotte Granfondo Alpes
-              </a>
-              .
+              <A href="https://lepapemarmottegranfondoalpes.com/">Marmotte Granfondo Alpes</A>.
             </li>
             <li>Designing short.is v3.</li>
             <li>
-              Architecture for{' '}
-              <a href="https://nivolet.cc" target="_blank" rel="noopener noreferrer">
-                nivolet.cc
-              </a>{' '}
-              - current thinking is SQLite, Node, GraphQL, Next.js and Mapbox.
+              Architecture for <A href="https://nivolet.cc">nivolet.cc</A> - current thinking is
+              SQLite, Node, Express, GraphQL, Next.js and Mapbox.
             </li>
             <li>
-              Learning Svelte, considering{' '}
-              <a href="https://sapper.svelte.dev/" target="_blank" rel="noopener noreferrer">
-                Sapper
-              </a>{' '}
-              as an alternative to Gatsby.
+              Learning Svelte, considering <A href="https://sapper.svelte.dev/">Sapper</A> as an
+              alternative to Gatsby.
             </li>
             <li>
               Reading{' '}
-              <a
-                href="https://www.amazon.co.uk/Lost-Connections-Youre-Depressed-Find/dp/1408878720"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+              <A href="https://www.amazon.co.uk/Lost-Connections-Youre-Depressed-Find/dp/1408878720">
                 Lost Connections: Why You’re Depressed and How to Find Hope
-              </a>{' '}
+              </A>{' '}
               by Johann Hari.
+            </li>
+            <li>
+              Attempting a{' '}
+              <A href="https://www.areaware.com/products/gradient-puzzle?variant=36263538436">
+                frankly ridiculous Jigsaw
+              </A>
+              .
             </li>
           </ul>
         </StyledPost>
