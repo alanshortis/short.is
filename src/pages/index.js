8352ac428b8b6feef08c08d134a2fec84632d203
Alan Shortis <alanshortis@Alans-MacBook-Pro.local>
2020-03-15 23:00:45 +0000

    Create a layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import SEO from '../components/SEO';
+import Layout from '../components/Layout';
 
-const Home = () => <>v3</>;
+const Home = () => (
+  <Layout>
+    <SEO />
+    <p>home</p>
+  </Layout>
+);
 
 export default Home;
