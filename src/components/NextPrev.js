b6d6ee6e4899a640eba04d913122a7d3a936cfe0
Alan Shortis <alanshortis@Alans-MacBook-Pro.local>
2020-03-17 22:10:48 +0000

    Post navigation component

---
+++ src/components/NextPrev.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+
+const NextPrev = ({ pageContext }) => {
+  const { newer, older } = pageContext;
+  return (
+    <div>
+      {newer && <Link to={newer.frontmatter.slug}>{newer.frontmatter.title}</Link>}
+      {older && <Link to={older.frontmatter.slug}>{older.frontmatter.title}</Link>}
+    </div>
+  );
+};
+
+NextPrev.propTypes = {
+  pageContext: PropTypes.shape({
+    newer: PropTypes.oneOf([
+      null,
+      PropTypes.shape({
+        frontmatter: PropTypes.shape({
+          slug: PropTypes.string,
+          title: PropTypes.string,
+        }),
+      }),
+    ]),
+    older: PropTypes.oneOf([
+      null,
+      PropTypes.shape({
+        frontmatter: PropTypes.shape({
+          slug: PropTypes.string,
+          title: PropTypes.string,
+        }),
+      }),
+    ]),
+  }).isRequired,
+};
+
+export default NextPrev;
