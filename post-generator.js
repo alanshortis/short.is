db4a131f648f06e32942acf2c50e2beb43763fde
Alan Shortis <alanshortis@Alans-MacBook-Pro.local>
2020-03-22 23:16:15 +0000

    Generator progress

--- post-generator.js
+++ post-generator.js
@@ -1,4 +1,6 @@
 const prompts = require('prompts');
+const fs = require('fs');
+const chalk = require('chalk');
 
 const postType = async () => {
   const response = await prompts({
@@ -24,14 +26,49 @@ const postTitle = async () => {
   return response.title;
 };
 
-const postGenerator = async () => {
-  const type = await postType();
+const createFolder = async folder => {
+  const fullPath = `src/posts/daily/${folder}`;
+  await fs.mkdirSync(fullPath, { recursive: true });
+  console.log(chalk.green(`✔ ${fullPath} folder created.`));
+};
 
-  console.log(type);
+const createFile = async (path, fileName, content) => {
+  await fs.writeFile(`${path}/${fileName}`, content, err => {
+    if (err) throw err;
+    console.log(chalk.green(`✔ ${path}/${fileName} created.`));
+  });
+};
+
+const postGenerator = async () => {
+  const today = new Date();
+  const date = today.getDate();
+  const month = today.getMonth();
+  const formattedMonth = String(month + 1).padStart(2, 0);
+  const year = today.getFullYear() + 2;
+  const formattedDate = `${year}-${formattedMonth}-${date}`;
+  const path = 'src/posts';
+
+  const type = await postType();
 
   if (type === 'writing') {
     const title = await postTitle();
-    console.log(title);
+    const slug = title
+      .split(' ')
+      .join('-')
+      .toLowerCase();
+
+    createFile(`${path}/writing`, `${formattedDate}.mdx`, slug);
+  }
+
+  if (type === 'daily') {
+    const firstDailyPost = new Date('2020-03-15');
+    const daysSince = (today - firstDailyPost) / (1000 * 3600 * 24);
+    const dailyPostCount = Math.round(daysSince - 1);
+    const yearMonthFolder = `${year}/${formattedMonth}`;
+    const fullPath = `${path}/daily/${yearMonthFolder}`;
+
+    await createFolder(yearMonthFolder);
+    await createFile(fullPath, `${formattedDate}.mdx`, dailyPostCount);
   }
 };
 
