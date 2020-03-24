feeab57aea6eb223caeca7f916533b8695509e9f
Alan Shortis <alan.shortis@experian.com>
2020-03-24 21:59:28 +0000

    Post creation (#63)
    * Add package and script
    
    * Post and generator progress
    
    * Add chalk
    
    * Generator progress
    
    * Add templates
    
    * Some simplification
    
    * Post progress
    
    * Remove post
    
    * Small updates
    
    * Move packages
    
    * Add post
    
    Co-authored-by: Alan Shortis <alanshortis@Alans-MacBook-Pro.local>

---
+++ post-generator.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+const prompts = require('prompts');
+const chalk = require('chalk');
+
+const POST_ROOT = 'src/posts';
+const FIRST_DAILY_POST = new Date('2020-03-15');
+
+const templates = {
+  daily: (date, title) => `---
+title: '#${title}'
+date: '${date}'
+mobile: false
+featured: false
+slug: '/daily/${title}'
+---`,
+
+  writing: (date, title, slug) => `---
+title: '${title}'
+date: '${date}'
+category: 'development'
+slug: '/writing/${slug}'
+intro: ""
+---`,
+};
+
+const postType = async () => {
+  const response = await prompts({
+    type: 'select',
+    name: 'type',
+    message: 'What type of post should be created?',
+    choices: [
+      { title: 'Daily', value: 'daily' },
+      { title: 'Writing', value: 'writing' },
+    ],
+  });
+
+  return response.type;
+};
+
+const postTitle = async () => {
+  const response = await prompts({
+    type: 'text',
+    name: 'title',
+    message: "What's the title of the new post?",
+  });
+
+  return response.title;
+};
+
+const createFolder = async folder => {
+  const fullPath = `${POST_ROOT}/daily/${folder}`;
+  if (!fs.existsSync(fullPath)) {
+    fs.mkdirSync(fullPath, { recursive: true });
+    console.log(chalk.green(`✔ ${fullPath} folder created.`));
+  }
+};
+
+const createFile = async (fileName, content, path = `${POST_ROOT}/writing`) => {
+  fs.writeFile(`${path}/${fileName}`, content, err => {
+    if (err) throw err;
+    console.log(chalk.green(`✔ ${path}/${fileName} created.`));
+  });
+};
+
+const postGenerator = async () => {
+  const today = new Date();
+  const date = String(today.getDate()).padStart(2, 0);
+  const month = String(today.getMonth() + 1).padStart(2, 0);
+  const year = today.getFullYear();
+  const formattedDate = `${year}-${month}-${date}`;
+
+  const type = await postType();
+
+  if (type === 'writing') {
+    const title = await postTitle();
+    const slug = encodeURI(
+      title
+        .split(' ')
+        .join('-')
+        .toLowerCase()
+    );
+
+    createFile(`${formattedDate}.mdx`, templates.writing(formattedDate, title, slug));
+  }
+
+  if (type === 'daily') {
+    const daysSince = (today - FIRST_DAILY_POST) / (1000 * 3600 * 24);
+    const dailyPostCount = Math.round(daysSince - 1);
+    const yearMonthFolder = `${year}/${month}`;
+    const fullPath = `${POST_ROOT}/daily/${yearMonthFolder}`;
+
+    await createFolder(yearMonthFolder);
+    await createFile(
+      `${formattedDate}.mdx`,
+      templates.daily(formattedDate, dailyPostCount),
+      fullPath
+    );
+  }
+};
+
+postGenerator();
