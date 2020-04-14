fadcbb6688f3bf706e7badc10700b17b113c7d9a
Alan Shortis <alanshortis@Alans-MacBook-Pro.local>
2020-04-14 18:49:51 +0100

    List playlists (temp)

--- src/pages/playlists.js
+++ src/pages/playlists.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/Layout';
 
-const Playlists = () => (
-  <Layout title="Playlists" pathName="/playlists">
-    <p>Playlists</p>
-  </Layout>
-);
+const playlists = [
+  { name: '99 albums', spotifyID: '7autJTV8V8OZlt7EG03EwM' },
+  { name: '2020', spotifyID: '7hSbxdvfTP9N2869hrjXfE' },
+];
+
+const Playlists = () => {
+  const data = useStaticQuery(
+    graphql`
+      fragment SpotifyFields on SpotifyPlaylist {
+        description
+        name
+        tracks {
+          total
+        }
+        external_urls {
+          spotify
+        }
+        images {
+          height
+          url
+          width
+        }
+      }
+      query {
+        thing: spotifyPlaylist(spotifyId: { eq: "7autJTV8V8OZlt7EG03EwM" }) {
+          ...SpotifyFields
+        }
+        thing2: spotifyPlaylist(spotifyId: { eq: "7hSbxdvfTP9N2869hrjXfE" }) {
+          ...SpotifyFields
+        }
+      }
+    `
+  );
+
+  return (
+    <Layout title="Playlists" pathName="/playlists">
+      <p>Playlists</p>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </Layout>
+  );
+};
 
 export default Playlists;
