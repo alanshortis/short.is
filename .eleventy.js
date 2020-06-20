6991a6c00ac88ec289b3e7d8fef53c52f0ec0b9c
Alan Shortis <alan.shortis@mac.com>
2020-06-20 22:25:25 +0100

    RSS feeds

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const eleventyRssPlugin = require('@11ty/eleventy-plugin-rss');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(eleventyRssPlugin);
   eleventyConfig.addLayoutAlias('main', 'main.njk');
-  eleventyConfig.setTemplateFormats(['njk', 'md', 'css', 'txt', 'json', 'js', 'png']);
+  eleventyConfig.setTemplateFormats(['njk', 'md', 'css', 'png']);
 
   return {
     dir: {
