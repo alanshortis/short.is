2c837a9657ff513086884557be385c7650b2f3bc
Alan Shortis <alan.shortis@mac.com>
2020-07-12 14:41:27 +0100

    CSS minification

---
+++ src/_data/site.js
@@ -0,0 +1,16 @@
+module.exports = {
+  isProd: process.env.ELEVENTY_ENV === 'production',
+  author: 'Alan Shortis',
+  email: 'hello@short.is',
+  title: 'Alan Shortis',
+  description: 'Alan Shortis is a Web Developer',
+  url: 'https://short.is',
+  twitter: '@alanshortis',
+  social: {
+    'https://github.com/alanshortis': 'GitHub',
+    'https://www.instagram.com/ashortis/': 'Instagram',
+    'https://www.last.fm/user/ashortis': 'Last.fm',
+    'https://www.strava.com/athletes/138800': 'Strava',
+    'https://twitter.com/alanshortis': 'Twitter',
+  },
+};
