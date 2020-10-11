e0073c5b5bfcd76d32e265578dac82ed667110c8
Alan Shortis <alan.shortis@mac.com>
2020-10-11 14:48:49 +0100

    Version to cache bust assets (#288)

---
+++ src/_data/site.js
@@ -0,0 +1,18 @@
+const { version } = require('../../package.json');
+
+module.exports = {
+  version,
+  author: 'Alan Shortis',
+  email: 'hello@short.is',
+  title: 'Alan Shortis',
+  description: 'Alan Shortis is a Web Developer',
+  url: 'https://short.is',
+  twitter: '@alanshortis',
+  social: {
+    'https://github.com/alanshortis': 'GitHub',
+    'https://www.instagram.com/ashortis/': 'Instagram',
+    'https://www.last.fm/user/ashortis': 'Last.fm',
+    'https://www.strava.com/athletes/138800': 'Strava',
+    'https://twitter.com/alanshortis': 'Twitter',
+  },
+};
