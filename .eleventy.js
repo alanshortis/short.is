87273f975f93d4cd1f7cd13fe863290564090c4f
Alan Shortis <alan.shortis@mac.com>
2020-10-17 23:21:23 +0100

    Syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 require('dotenv-safe').config();
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const htmlmin = require('html-minifier');
 const filters = require('./filters');
 
 module.exports = eleventyConfig => {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(syntaxHighlight, { templateFormats: ['md'] });
 
   eleventyConfig.addLayoutAlias('main', 'layouts/main.njk');
   eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
