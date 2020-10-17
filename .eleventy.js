28a67b94ffd328b82a2481922f8aa2ab5cfdb5f6
Alan Shortis <alan.shortis@mac.com>
2020-10-17 22:20:57 +0100

    Minify HTML

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,25 @@
 require('dotenv-safe').config();
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const htmlmin = require('html-minifier');
 
 module.exports = eleventyConfig => {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   eleventyConfig.addLayoutAlias('main', 'layouts/main.njk');
+
+  eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
+    console.log(process.env);
+    if (outputPath.endsWith('.html')) {
+      const minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
 
   return {
     dir: {
