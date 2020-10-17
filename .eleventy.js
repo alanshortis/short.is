b60543d325890e98707d0ed23e285a72bff878d0
Alan Shortis <alan.shortis@mac.com>
2020-10-17 21:05:25 +0100

    Navigation items

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 require('dotenv-safe').config();
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
 
 module.exports = eleventyConfig => {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
   eleventyConfig.addLayoutAlias('main', 'layouts/main.njk');
 
   return {
