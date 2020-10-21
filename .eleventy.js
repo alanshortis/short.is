8eba86fc8d8326b4ecd23220311313aac9d8d187
Alan Shortis <alan.shortis@mac.com>
2020-10-21 23:02:33 +0100

    Add 404 page

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
 require('dotenv-safe').config();
+const fs = require('fs');
 const navigationPlugin = require('@11ty/eleventy-navigation');
 const rssPlugin = require('@11ty/eleventy-plugin-rss');
 const syntaxPlugin = require('@11ty/eleventy-plugin-syntaxhighlight');
 const htmlmin = require('html-minifier');
 const filters = require('./filters');
+
+const input = 'src';
+const output = 'dist';
 
 module.exports = eleventyConfig => {
   eleventyConfig.addPlugin(navigationPlugin);
@@ -11,9 +15,11 @@ module.exports = eleventyConfig => {
   eleventyConfig.addPlugin(syntaxPlugin, { templateFormats: ['md'] });
 
   eleventyConfig.setTemplateFormats(['md', 'njk']);
-  eleventyConfig.addPassthroughCopy('src/assets');
 
+  // Do not use gotignore to determne files that shouldn't be copied on
+  // build otherwise our compiled CSS and JS will never go anywhere.
   eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.addPassthroughCopy(`${input}/assets`);
 
   eleventyConfig.addLayoutAlias('main', 'layouts/main.njk');
   eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
@@ -22,6 +28,7 @@ module.exports = eleventyConfig => {
   eleventyConfig.addFilter('formatDate', filters.dateFormat);
   eleventyConfig.addFilter('dateDiff', filters.dateDiff);
 
+  // Minify HTML
   eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
     if (outputPath.endsWith('.html')) {
       const minified = htmlmin.minify(content, {
@@ -35,10 +42,24 @@ module.exports = eleventyConfig => {
     return content;
   });
 
+  // Enable 404 for local development
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function (err, browserSync) {
+        const fourOhFour = fs.readFileSync(`${output}/404/index.html`);
+
+        browserSync.addMiddleware('*', (req, res) => {
+          res.write(fourOhFour);
+          res.end();
+        });
+      },
+    },
+  });
+
   return {
     dir: {
-      input: 'src',
-      output: 'dist',
+      input,
+      output,
     },
   };
 };
