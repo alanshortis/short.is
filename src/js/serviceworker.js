92789075c89f433f81bf49a2234064d67e3029e9
Alan Shortis <alan.shortis@mac.com>
2020-10-24 21:43:33 +0100

    Simplest serviceworker

--- src/js/serviceworker.js
+++ src/js/serviceworker.js
@@ -1,1 +1,37 @@
-console.log('Service Worker?');
+// HTML files: try the network first, then the cache.
+// Other files: try the cache first, then the network.
+// Both: cache a fresh version if possible.
+// TODO: A lot. There is no clean-up of cache here.
+
+import { version } from '../../package.json';
+
+const cacheName = 'short.is' + version;
+
+addEventListener('fetch', fetchEvent => {
+  const request = fetchEvent.request;
+  if (request.method !== 'GET') {
+    return;
+  }
+  fetchEvent.respondWith(
+    (async function () {
+      const responseFromFetch = fetch(request);
+      fetchEvent.waitUntil(
+        (async function () {
+          const responseCopy = (await responseFromFetch).clone();
+          const myCache = await caches.open(cacheName);
+          await myCache.put(request, responseCopy);
+        })()
+      );
+      if (request.headers.get('Accept').includes('text/html')) {
+        try {
+          return await responseFromFetch;
+        } catch (error) {
+          return caches.match(request);
+        }
+      } else {
+        const responseFromCache = await caches.match(request);
+        return responseFromCache || responseFromFetch;
+      }
+    })()
+  );
+});
