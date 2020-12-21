b0a80647c0ce6c0f1fd5424fa2d3d65d37b80dee
Alan Shortis <alan.shortis@mac.com>
2020-12-21 16:55:22 +0000

    Configure webpack

---
+++ src/js/sw.js
@@ -0,0 +1,3 @@
+import test from './func';
+
+test('ayoooooo');
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = ({ NODE_ENV }) => {
+  const isProd = NODE_ENV === 'production';
+
+  return {
+    mode: NODE_ENV,
+    watch: !isProd,
+    devtool: isProd ? false : 'source-map',
+    entry: {
+      app: './src/js/app.js',
+      sw: './src/js/sw.js',
+    },
+    output: {
+      path: path.resolve(__dirname, 'src'),
+      filename: '[name].js',
+    },
+  };
+};
