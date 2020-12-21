fd456ccbe3a235fa7ede775766795f7957a78242
Alan Shortis <alan.shortis@mac.com>
2020-12-21 16:27:24 +0000

    Feature/post content (#316)
    * Add existing posts
    
    * Add a writing layout
    
    * List posts
    
    * Add date filters
    
    * Formatted post date
    
    * Rename post layout
    
    * Post page data

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,15 @@
+const filters = require('./filters');
+
 const input = 'src';
 const output = 'dist';
 
 module.exports = eleventyConfig => {
   eleventyConfig.addLayoutAlias('main', 'layouts/main.njk');
   eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
+  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+
+  eleventyConfig.addFilter('dateFormat', filters.dateFormat);
+  eleventyConfig.addFilter('dateDiff', filters.dateDiff);
 
   return {
     dir: {
