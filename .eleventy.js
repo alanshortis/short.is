b401bb4046ec79312ce0210ae2dc44e7a7680344
Alan Shortis <alan.shortis@mac.com>
2020-12-21 14:50:58 +0000

    Add layouts

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ const input = 'src';
 const output = 'dist';
 
 module.exports = eleventyConfig => {
+  eleventyConfig.addLayoutAlias('main', 'layouts/main.njk');
+  eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
+
   return {
     dir: {
       input,
