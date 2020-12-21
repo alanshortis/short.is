5c2199c9c8cc46d536a443f7ae6d5490a6643d68
Alan Shortis <alan.shortis@mac.com>
2020-12-21 14:59:41 +0000

    Add a writing layout

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ const output = 'dist';
 module.exports = eleventyConfig => {
   eleventyConfig.addLayoutAlias('main', 'layouts/main.njk');
   eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
+  eleventyConfig.addLayoutAlias('writing', 'layouts/writing.njk');
 
   return {
     dir: {
