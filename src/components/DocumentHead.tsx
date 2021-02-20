60d1079cd59f0b9b96e4e53fc260b3cfd2da90fd
Alan Shortis <alan.shortis@mac.com>
2021-02-20 18:21:59 +0000

    Add meta tags to document

--- src/components/DocumentHead.tsx
+++ src/components/DocumentHead.tsx
@@ -1,8 +1,20 @@
 import Head from 'next/head';
+import type Meta from '../types/meta';
 
-const DocumentHead = () => (
+interface Props {
+  meta: Meta;
+}
+
+const DocumentHead: React.FC<Props> = ({ meta }) => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content={meta.description} />
+    <meta property="og:title" content={meta.title} />
+    <meta property="og:description" content={meta.description} />
+    <meta property="og:url" content={meta.url} />
+    {/* <meta property="og:image" content="/img/og.png" /> */}
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:creator" content={meta.twitter} />
   </Head>
 );
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,30 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { StrictMode } from 'react';
 import { ThemeProvider } from 'styled-components';
 import type { AppProps } from 'next/app';
 import DocumentHead from '../components/DocumentHead';
 import defaultTheme from '../styles/defaultTheme';
 
-const Shortis = ({ Component, pageProps }: AppProps) => (
-  <StrictMode>
-    <ThemeProvider theme={defaultTheme}>
-      <DocumentHead />
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
-    </ThemeProvider>
-  </StrictMode>
-);
+const Shortis = ({ Component, pageProps }: AppProps) => {
+  const props = {
+    ...pageProps,
+    meta: {
+      description: 'Alan Shortis is a front end developer',
+      title: 'Alan Shortis',
+      twitter: '@alanshortis',
+      url: 'https://short.is',
+      year: new Date().getFullYear(),
+    },
+  };
+
+  return (
+    <StrictMode>
+      <ThemeProvider theme={defaultTheme}>
+        <DocumentHead {...props} />
+        <Component {...props} />
+      </ThemeProvider>
+    </StrictMode>
+  );
+};
 
 export default Shortis;
