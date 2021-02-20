95647a9d94597b45903644d199772c690bb55394
Alan Shortis <alan.shortis@mac.com>
2021-02-20 15:13:45 +0000

    Test disabling client JS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
-const Home = () => <p>Hello</p>;
+export const config = {
+  unstable_runtimeJS: false
+};
+
+const Home = () => <p>This is v4 of short.is, the development of which is going to be public.</p>;
 
 export default Home;
