12a3fbd29f152cc69b24330a012fb37db9f21a8f
Alan Shortis <alan.shortis@mac.com>
2021-02-23 17:00:20 +0000

    Shift stuff around

--- src/data/Meta.ts
+++
@@ -1,9 +0,0 @@
-const meta = {
-  description: 'Alan Shortis is a front end developer',
-  title: 'Alan Shortis',
-  twitter: '@alanshortis',
-  url: 'https://short.is',
-  year: new Date().getFullYear(),
-};
-
-export default meta;
--- src/data/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as goodreads } from './goodreads';
-export { default as meta } from './meta';
