d5273310fd2b52ceb36d70d2362e549d5d9011fa
Alan Shortis <alan.shortis@mac.com>
2021-02-27 20:45:58 +0000

    Date format helper

---
+++ src/helpers/date-format.ts
@@ -0,0 +1,9 @@
+const dateFormat = (date: string): string => {
+  return new Date(date).toLocaleDateString('en-GB', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+  });
+};
+
+export default dateFormat;
