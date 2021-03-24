cc19c258dcfd3fc43886bcb54ea9bb61d4ca777a
Alan Shortis <alan.shortis@mac.com>
2021-03-24 22:41:35 +0000

    Post list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,25 @@
-const Home = () => <p>short.is v4</p>;
+import getPosts from '../data/posts';
+import PostList from '../components/PostList';
+
+const Home = ({ posts }) => (
+  <>
+    <p>short.is v4</p>
+    <PostList posts={posts} />
+  </>
+);
 
 export const config = {
   unstable_runtimeJS: false,
 };
+
+export async function getStaticProps() {
+  const allPosts = getPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
+}
 
 export default Home;
