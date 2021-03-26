6baf21ff9cdc95d578ac2b6dc19b529a56c065e2
Alan Shortis <alan.shortis@mac.com>
2021-03-26 18:30:23 +0000

    Fix link URL

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -5,7 +5,7 @@ const PostList = ({ posts }) => (
   <ul>
     {posts.map(({ slug, date, title, intro }) => (
       <li key={slug}>
-        <Link href={`writing/${slug}`}>
+        <Link href={`/writing/${slug}`}>
           <a>
             <PostDate date={date} />
             <h2>{title}</h2>
