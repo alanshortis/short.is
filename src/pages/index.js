07f3609f36255ffe9629bf6029f5f80e7af80f49
Alan Shortis <alan.shortis@mac.com>
2021-03-27 18:51:09 +0000

    feature/goodreads (#358)
    * Deps
    
    * Environment variables
    
    * Fetch data from goodreads
    
    * Create about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
+import Link from 'next/link';
 import { allPostFrontMatter } from '../data/posts';
 import { Layout, PostList } from '../components';
 
 const Home = ({ meta, posts }) => (
   <Layout meta={meta}>
+    <Link href="/about">
+      <a>Tell me more &rarr;</a>
+    </Link>
     <PostList posts={posts} />
   </Layout>
 );
