5225552b3ed4c47ef2aa33520320cc23f644a53c
Alan Shortis <alan.shortis@mac.com>
2021-03-27 21:55:27 +0000

    Page props

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 import { allPostFrontMatter } from '../data/posts';
 import { Layout, PostList } from '../components';
 
 const Home = ({ meta, posts }) => (
   <Layout meta={meta}>
+    <h1>Alan Shortis is a front end developer</h1>
     <Link href="/about">
       <a>Tell me more &rarr;</a>
     </Link>
@@ -22,5 +24,25 @@ export async function getStaticProps() {
     },
   };
 }
+
+Home.propTypes = {
+  meta: PropTypes.shape({
+    description: PropTypes.string,
+    title: PropTypes.string,
+    twitter: PropTypes.string,
+    url: PropTypes.string,
+    year: PropTypes.number,
+  }).isRequired,
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.string,
+      intro: PropTypes.string,
+      slug: PropTypes.string,
+      title: PropTypes.string,
+      nextPostSlug: PropTypes.string,
+      prevPostSlug: PropTypes.string,
+    })
+  ),
+};
 
 export default Home;
