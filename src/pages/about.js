b33b743876046f6cc74617823f3753db8a85d9d9
Alan Shortis <alan.shortis@mac.com>
2021-03-27 19:00:17 +0000

    feature/render-books (#359)
    * Create goodreads component
    
    * Render books now reading

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,18 @@
 import getGoodreads from '../data/goodreads';
+import { Goodreads } from '../components';
 
 const About = ({ goodreads }) => {
-  return <h1>About</h1>;
+  return (
+    <>
+      <h1>About</h1>
+      <h2>Now</h2>
+      <ul>
+        <li>
+          <Goodreads goodreads={goodreads} />
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export async function getStaticProps() {
