1f818e8d926761a0da1d8c7de713c416646726a9
Alan Shortis <alan.shortis@mac.com>
2021-03-28 00:34:59 +0000

    External links in markdown

--- src/pages/writing/[slug].js
+++ src/pages/writing/[slug].js
@@ -1,9 +1,10 @@
-import Link from 'next/link';
 import renderToString from 'next-mdx-remote/render-to-string';
 import hydrate from 'next-mdx-remote/hydrate';
+import externalLinks from 'remark-external-links';
 import { allPostFrontMatter, postContent } from '../../data/posts';
 import { ExampleEmbed, Layout, PostDate, PostNav } from '../../components';
 
+// Add each component used in MDX files.
 const components = { ExampleEmbed };
 
 const Post = ({ content, frontMatter, meta, nextPost, prevPost }) => {
@@ -41,7 +42,12 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const { content, frontMatter, nextPost, prevPost } = postContent(params.slug);
-  const mdxContent = await renderToString(content, { components });
+  const mdxContent = await renderToString(content, {
+    components,
+    mdxOptions: {
+      remarkPlugins: [externalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
+    },
+  });
 
   return { props: { content: mdxContent, frontMatter, nextPost, prevPost } };
 }
