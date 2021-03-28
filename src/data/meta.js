f9ea95ae2f5e35fdf3f9b2cbe49c222017c54a2c
Alan Shortis <alan.shortis@mac.com>
2021-03-28 15:57:11 +0100

    feature/rss (#362)
    * Add some meta
    
    * Create feed
    
    * Generate feed when rendering posts
    
    * Create folders recursively

--- src/data/meta.js
+++ src/data/meta.js
@@ -1,5 +1,7 @@
 const meta = {
+  author: 'Alan Shortis',
   description: 'Alan Shortis is a front end developer',
+  email: 'hello@short.is',
   title: 'Alan Shortis',
   twitter: '@alanshortis',
   url: 'https://short.is',
