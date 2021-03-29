ffe9c5a60d1503f504f3abc3fd3b34ce56672ed3
Alan Shortis <alan.shortis@mac.com>
2021-03-29 11:49:21 +0100

    Add web component to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,34 @@
+import { useEffect } from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
 
-const Header = () => (
-  <header>
-    <div>
-      <Link href="/">
-        <a>
-          <span>Home page</span>
-        </a>
-      </Link>
-    </div>
-    <div>
-      <Link href="/">
-        <a>Logo</a>
-      </Link>
-    </div>
-    <button>Toggle</button>
-  </header>
-);
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Header = () => {
+  useEffect(() => {
+    import('../web-components/dark-mode');
+  });
+
+  return (
+    <StyledHeader>
+      <div>
+        <Link href="/">
+          <a>
+            <span>Home page</span>
+          </a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/">
+          <a>Logo</a>
+        </Link>
+      </div>
+      <dark-mode></dark-mode>
+    </StyledHeader>
+  );
+};
 
 export default Header;
