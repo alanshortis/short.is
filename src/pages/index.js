b426f74ed215d5d6105fc9e85b68830aff0226d1
Alan Shortis <alan.shortis@mac.com>
2021-05-29 15:23:49 +0100

    List posts on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,26 @@
+import Link from 'next/link';
+import { allPostFrontMatter } from '../data/posts';
 import { Layout } from '../components';
 
-const Home = ({ meta }) => (
+export async function getStaticProps() {
+  return {
+    props: {
+      posts: allPostFrontMatter,
+    },
+  };
+}
+
+const Home = ({ meta, posts }) => (
   <Layout meta={meta}>
-    <p>Home</p>
+    {posts.map(post => (
+      <Link key={post.slug} href={`/writing/${post.slug}`}>
+        <a>
+          <time dateTime={post.date}>{post.date}</time>
+          <h2>{post.title}</h2>
+          <p>{post.intro}</p>
+        </a>
+      </Link>
+    ))}
   </Layout>
 );
 
