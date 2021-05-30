e15abddf1369d651661600f1c8dc22d70455f195
Alan Shortis <alan.shortis@mac.com>
2021-05-30 13:16:22 +0100

    feature/dark-mode-prod (#377)
    * Make SSR conditonal
    
    * Remove needless id

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 
 const SchemeToggle = dynamic(() => import('../components/SchemeToggle'), {
-  ssr: false,
+  ssr: process.env.NODE_ENV === 'production',
 });
 
 const Header = () => (
