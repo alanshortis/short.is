d2cd096459a400b7fe8a34c5c5e7ba7f69461a60
Alan Shortis <alan.shortis@mac.com>
2021-06-01 23:22:13 +0100

    Updated label

--- src/components/PostDate.js
+++ src/components/PostDate.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { dateFormat, daysSince } from '../helpers';
+import { Label } from '../components';
 
 const StyledSpan = styled(Label)`
   display: flex;
@@ -7,11 +8,20 @@ const StyledSpan = styled(Label)`
   margin-bottom: calc(${p => p.theme.spacing} / 4);
 `;
 
-const PostDate = ({ date }) => (
-  <StyledSpan>
-    <time dateTime={date}>{dateFormat(date)}</time>
-    {daysSince(date) < 100 && <span>NEW</span>}
-  </StyledSpan>
-);
+const PostDate = ({ date, updated }) => {
+  const withLabel = () => {
+    if (daysSince(date) < 28) return <span>New</span>;
+    if (daysSince(updated) < 28) return <span>Updated</span>;
+
+    return '';
+  };
+
+  return (
+    <StyledSpan>
+      <time dateTime={date}>{dateFormat(date)}</time>
+      {withLabel()}
+    </StyledSpan>
+  );
+};
 
 export default PostDate;
