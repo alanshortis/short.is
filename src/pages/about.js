743930633fce15b4edde24fb1533cc1a77471232
Alan Shortis <alan.shortis@mac.com>
2021-06-11 23:11:17 +0100

    Link to article from about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import getGoodreads from '../data/goodreads';
 import getLastfm from '../data/lastfm';
 import { A, Goodreads, Layout, Lastfm } from '../components';
@@ -22,28 +23,34 @@ const About = ({ goodreads, lastFm, meta }) => (
       <PostMeta>
         <h1>About</h1>
         <p className="intro">
-          I’m Alan Shortis—a front end developer based in Nottingham. I like CSS more than
-          JavaScript, design systems more than chaos, and accessibility more than exclusion.
+          I’m Alan Shortis—a front end developer based in Nottingham. I like CSS more than JavaScript, design
+          systems more than chaos, and accessibility more than exclusion.
         </p>
       </PostMeta>
       <PostBody>
         <p>
-          I specialise in building user interfaces with HTML, CSS, and JavaScript. I have been a
-          developer professionally in some capacity for almost 15 years, though I made my first
-          websites in the late 90s. They were terrible, even by the standards of Geocities.
+          I specialise in building user interfaces with HTML, CSS, and JavaScript. I have been a developer
+          professionally in some capacity for almost 15 years, though I made my first websites in the late
+          90s. They were terrible, even by the standards of Geocities.
         </p>
         <p>
-          My preference for front end development was founded in being a design hobbyist who used
-          the internet to show off derivative ideas. Across my career I have also worked with
-          Classic ASP, .net, PHP, and SQL in my brief time as an analyst, but building accessible
-          user interfaces for people is where I have settled.
+          My preference for front end development was founded in being a design hobbyist who used the internet
+          to show off derivative ideas. Across my career I have also worked with Classic ASP, .net, PHP, and
+          SQL in my brief time as an analyst, but building accessible user interfaces for people is where I
+          have settled.
         </p>
         <p>
           This site uses <A href="https://nextjs.org/">Next.js</A> to generate static HTML,{' '}
           <A href="https://styled-components.com/">styled components</A> to author CSS,{' '}
-          <A href="https://developer.mozilla.org/en-US/docs/Web/Web_Components">web components</A>{' '}
-          for minimal client JavaScript, and <A href="https://www.netlify.com/">Netlify</A> to
-          build, deploy and host.
+          <A href="https://developer.mozilla.org/en-US/docs/Web/Web_Components">web components</A> for minimal
+          client JavaScript, and <A href="https://www.netlify.com/">Netlify</A> to build, deploy and host.
+        </p>
+        <p>
+          You can read more about how this site's performance is so good in{' '}
+          <Link href="/writing/next.js-with-web-components-and-a-0kb-bundle">
+            <a>Next.js with web components and a 0kb bundle</a>
+          </Link>
+          .
         </p>
         <p>
           Find me on{' '}
