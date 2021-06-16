a3e215d8098966dee2de215cb439059c88750021
Alan Shortis <alan.shortis@mac.com>
2021-06-16 23:42:27 +0100

    Add visually hidden style

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -117,6 +117,16 @@ const GlobalStyles = createGlobalStyle`
   h4, .h4 {
     font-size: 1.44rem;
   }
+
+  .visually-hidden {
+    clip: rect(0 0 0 0);
+    clip-path: inset(50%);
+    height: 1px;
+    overflow: hidden;
+    position: absolute;
+    white-space: nowrap;
+    width: 1px;
+  }
 `;
 
 export default GlobalStyles;
