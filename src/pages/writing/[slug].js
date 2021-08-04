838569a920b53d8c86c4e06118d56ea05c4906ef
alanshortis <alan.shortis@mac.com>
2021-08-04 13:17:28 +0100

    Create an array of headings for TOC

--- src/pages/writing/[slug].js
+++ src/pages/writing/[slug].js
@@ -4,6 +4,7 @@ import externalLinks from 'remark-external-links';
 import highlight from 'remark-highlight.js';
 import codeExtra from 'remark-code-extra';
 import remarkSlug from 'remark-slug';
+import GithubSlugger from 'github-slugger';
 import { allPostFrontMatter, postContent } from '../../data/posts';
 import { PostArticle, PostMeta, PostBody } from '../../components/PostLayout';
 import SyntaxStyles from '../../styles/SyntaxStyles';
@@ -25,6 +26,7 @@ export function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
+  // Get the content of the post.
   const { content, frontMatter, nextPost, prevPost } = postContent(params.slug);
   const mdxContent = await serialize(content, {
     mdxOptions: {
@@ -52,7 +54,22 @@ export async function getStaticProps({ params }) {
     },
   });
 
-  return { props: { mdxContent, frontMatter, nextPost, prevPost } };
+  // Create an array of all the h2 titles for a TOC.
+  const slugger = new GithubSlugger();
+  const headingsRegex = new RegExp(/^(## )(.*)\n/, 'gm'); // All h2 titles from markdown.
+  const headings = [...content.matchAll(headingsRegex)];
+
+  const toc = headings.map(heading => {
+    const title = heading[2].trim();
+    const id = slugger.slug(title);
+
+    return {
+      title,
+      id
+    }
+  })
+
+  return { props: { mdxContent, frontMatter, nextPost, prevPost, toc } };
 }
 
 export const config = {
@@ -60,9 +77,9 @@ export const config = {
 };
 
 // Add each component used in MDX files.
-const components = { ExampleEmbed, TableOfContents };
+const components = { ExampleEmbed };
 
-const Post = ({ mdxContent, frontMatter, meta, nextPost, prevPost }) => {
+const Post = ({ mdxContent, frontMatter, meta, nextPost, prevPost, toc }) => {
   const { date, title, intro, updated } = frontMatter;
   const isOld = daysSince(date) >= 365 * 2;
 
@@ -77,6 +94,7 @@ const Post = ({ mdxContent, frontMatter, meta, nextPost, prevPost }) => {
         </PostMeta>
         <PostBody>
           <p className="intro">{intro}</p>
+          {toc?.length > 1 && <TableOfContents sections={toc} />}
           <Mdx {...mdxContent} components={components} />
         </PostBody>
       </PostArticle>
