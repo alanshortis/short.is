f3bd1d1751df7e877408def4bccd5ec878802da9
alanshortis <alan.shortis@mac.com>
2021-08-04 14:40:16 +0100

    List styling

--- src/components/TableOfContents.js
+++ src/components/TableOfContents.js
@@ -1,11 +1,14 @@
 const TableOfContents = ({ sections }) => (
-  <nav>
-    <ol>
-      {sections.map(section => (
-        <li key={section.id}><a href={section.id}>{section.title}</a></li>
-      ))}
-    </ol>
-  </nav>
-)
+  <>
+    <h2 className="visually-hidden">Table of contents</h2>
+    <nav>
+      <ol>
+        {sections.map(section => (
+          <li key={section.id}><a href={`#${section.id}`}>{section.title}</a></li>
+        ))}
+      </ol>
+    </nav>
+  </>
+);
 
 export default TableOfContents;
