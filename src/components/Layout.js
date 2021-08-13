5e7e25b5b28cbbc235a051489ccd60a4b6567d05
alanshortis <alan.shortis@mac.com>
2021-08-13 17:30:14 +0100

    Use context in components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,12 @@
-import { useContext } from 'react';
 import { Head, Header, Footer, Main } from '../components';
-import MetaContext from '../data/meta';
 
-const Layout = ({ children, title, intro, hasFooter }) => {
-  const meta = useContext(MetaContext);
-  return (
-    <>
-      <Head title={title} intro={intro} meta={meta} />
-      <Header />
-      <Main>{children}</Main>
-      {hasFooter && <Footer meta={meta} />}
-    </>
-  );
-}
+const Layout = ({ children, title, intro, hasFooter }) => (
+  <>
+    <Head title={title} intro={intro} />
+    <Header />
+    <Main>{children}</Main>
+    {hasFooter && <Footer />}
+  </>
+);
 
 export default Layout;
