1d79999c33491490c1163c61eb44ad5b63f002e7
alanshortis <alan.shortis@mac.com>
2021-08-13 17:13:36 +0100

    USe meta context

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,17 @@
+import { useContext } from 'react';
 import { Head, Header, Footer, Main } from '../components';
+import MetaContext from '../data/meta';
 
-const Layout = ({ children, title, intro, meta, hasFooter }) => (
-  <>
-    <Head title={title} intro={intro} meta={meta} />
-    <Header />
-    <Main>{children}</Main>
-    {hasFooter && <Footer meta={meta} />}
-  </>
-);
+const Layout = ({ children, title, intro, hasFooter }) => {
+  const meta = useContext(MetaContext);
+  return (
+    <>
+      <Head title={title} intro={intro} meta={meta} />
+      <Header />
+      <Main>{children}</Main>
+      {hasFooter && <Footer meta={meta} />}
+    </>
+  );
+}
 
 export default Layout;
