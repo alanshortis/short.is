27f53b9ad34f2b6ebce8800e2cf545df183ca527
alanshortis <alan.shortis@mac.com>
2021-10-27 19:59:23 +0100

    Remove old site

--- src/helpers/daysSince.js
+++
@@ -1,8 +0,0 @@
-const daysSince = date => {
-  const today = new Date().getTime();
-  const postDate = new Date(date).getTime();
-
-  return Math.floor((today - postDate) / (1000 * 3600 * 24));
-};
-
-export default daysSince;
