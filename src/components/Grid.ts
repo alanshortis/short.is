e0d5ae4ed3bce957bbc1c2481692a5ad0a00742f
alanshortis <alan.shortis@mac.com>
2021-12-08 21:29:22 +0000

    Fix grid gap

--- src/components/Grid.ts
+++ src/components/Grid.ts
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
 
 export const Grid = styled.article`
-  column-gap: calc(var(--spacing) * 2);
   display: grid;
+  gap: var(--spacing);
   grid-template-columns: repeat(2, minmax(0, 1fr));
-  row-gap: var(--spacing);
   @media ${p => p.theme.media.medium} {
     grid-template-columns: repeat(12, minmax(0, 1fr));
   }
