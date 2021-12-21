8db0807acf83683a6e59da532fb009dcf810122d
alanshortis <alan.shortis@mac.com>
2021-12-21 18:14:12 +0000

    Environment variables

--- src/data/cloudinary.ts
+++ src/data/cloudinary.ts
@@ -4,12 +4,12 @@ export const allPhotos = async (folder: string): Promise<Photo[]> => {
   const folderNameLen = folder.length;
 
   const data = await fetch(
-    `https://api.cloudinary.com/v1_1/alanshortis/resources/image/upload?prefix=${folder}&context=true`,
+    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_NAME}/resources/image/upload?prefix=${folder}&context=true`,
     {
       headers: {
-        Authorization: `Basic ${Buffer.from('172199416163279:M-ZYbcFmIThD59EnYcxlz4V-wig').toString(
-          'base64'
-        )}`,
+        Authorization: `Basic ${Buffer.from(
+          `${process.env.CLOUDINARY_KEY}:${process.env.CLOUDINARY_SECRET}`
+        ).toString('base64')}`,
       },
     }
   ).then(res => res.json());
