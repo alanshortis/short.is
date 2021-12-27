f061606ad3594435a97f413118f05c49f50e38d2
alanshortis <alan.shortis@mac.com>
2021-12-27 18:10:51 +0000

    Add new post generator

---
+++ new.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-octal-escape */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-console */
+const fs = require('fs');
+const readline = require('readline');
+
+const PATH = 'src/posts';
+
+const template = (title, date) => `---
+title: '${title}'
+date: '${date}'
+intro: ""
+---
+`;
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("🤔   \x1B[34mWhat's the title of the post? \033[37m", title => {
+  const slug = encodeURIComponent(title.split(' ').join('-').toLowerCase());
+  const date = new Date().toISOString().slice(0, 10);
+
+  fs.writeFileSync(`${PATH}/${slug}.mdx`, template(title, date));
+  rl.close();
+});
+
+rl.on('close', () => {
+  console.log('🎉   \x1b[32mNew file created!');
+  process.exit(0);
+});
