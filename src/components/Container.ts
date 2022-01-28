e8b3fc3c538080e4b23777d13c3cfeb92242e3eb
Alan Shortis <alan.shortis@mac.com>
2022-01-28 20:52:24 +0000

    Simplify padding

--- src/components/Container.ts
+++ src/components/Container.ts
@@ -3,12 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   margin: 0 auto;
   max-width: 90rem;
-  padding: 0 calc(var(--spacing) / 2);
+  padding: 0 var(--spacing);
+  padding: 0 clamp(calc(var(--spacing) / 2), 5vw, calc(var(--spacing) * 2));
   width: 100%;
-  @media ${p => p.theme.media.small} {
-    padding: 0 var(--spacing);
-  }
-  @media ${p => p.theme.media.medium} {
-    padding: 0 calc(var(--spacing) * 2);
-  }
 `;
