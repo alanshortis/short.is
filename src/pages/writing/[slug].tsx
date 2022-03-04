092c2810edb3641a10bbcd28fe020c8b8c6f10da
Alan Shortis <alan.shortis@mac.com>
2022-03-04 22:05:25 +0000

    Fix lint issue

--- src/pages/writing/[slug].tsx
+++ src/pages/writing/[slug].tsx
@@ -41,7 +41,7 @@ export const config = {
 
 type PostProps = Omit<Post, 'slug' | 'content'>;
 
-const WrtingPost: FC<PostProps> = ({ title, date, intro, nextPost, prevPost, mdxContent, year }) => (
+const WrtingPost: FC<PostProps> = ({ title, date, intro, nextPost, prevPost, mdxContent }) => (
   <>
     <Head>
       {nextPost && <link rel="prefetch" href={`/writing/${nextPost.slug}`} />}
