2fb8933fec8dce4110569f11bd6877ef4b7b2b9b
Alan Shortis <alan.shortis@mac.com>
2022-03-26 21:11:38 +0000

    Test

---
+++ src/pages/asdf.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+export const config = {
+  unstable_runtimeJS: false,
+};
+
+const Asdf: NextPage = () => <img src="https://shortis.s3.eu-west-2.amazonaws.com/x1-0015.jpg" alt="pls" />;
+
+export default Asdf;
