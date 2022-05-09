b4c24be416f4a3ca1c9db6eb5e9d04123724ce73
Alan Shortis <alan.shortis@mac.com>
2022-05-10 00:06:00 +0100

    feature/links (#457)
    * Update links
    
    * Tabular nums

--- src/components/Label.ts
+++ src/components/Label.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Label = styled.span<{ toTheRight?: boolean }>`
   display: block;
   font-size: 0.8rem;
+  font-variant-numeric: tabular-nums;
   font-variation-settings: 'wght' ${p => p.theme.font.weight};
   letter-spacing: 2px;
   line-height: 1.563;
