19cf34f00600328ce545839a50b4e19d59bd4419
Alan <alanshortis@monzo.com>
2022-08-20 23:35:42 +0100

    Refactor styles

---
+++ src/components/Active.ts
@@ -0,0 +1,15 @@
+import { css } from 'styled-components';
+
+export const activeMarker = css`
+  --dot: 7px;
+  --half-dot: 3.5px;
+  background-color: currentColor;
+  border-radius: var(--dot);
+  content: '';
+  cursor: default;
+  height: var(--dot);
+  left: calc(50% - var(--half-dot));
+  position: absolute;
+  top: var(--spacing);
+  width: var(--dot);
+`;
