210d433770e974886641210833745c24307a7034
Alan Shortis <alan.shortis@mac.com>
2022-09-02 22:23:54 +0100

    feature/generator-update (#467)
    * Test command
    
    * Create a new branch
    
    * Create a branch
    
    * Error handling

--- new-post/index.js
+++ new-post/index.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const fs = require('fs/promises');
+const fsPromises = require('fs/promises');
+const fs = require('fs');
 const { exec } = require('child_process');
 const path = require('path');
 const prompts = require('prompts');
 const template = require('./templates');
 
 const PATH = path.join(__dirname, '../', 'src/posts');
-const streak = Math.ceil((new Date().getTime() - new Date('2022-07-17').getTime()) / (1000 * 3600 * 24));
+const streak = Math.ceil((new Date().getTime() - new Date('2022-08-17').getTime()) / (1000 * 3600 * 24));
 const today = new Date().toISOString().slice(0, 10);
 
 const createPost = async (type, title) => {
@@ -15,8 +16,13 @@ const createPost = async (type, title) => {
   const fileName = type === 'daily' ? today : encodeURIComponent(title.split(' ').join('-').toLowerCase());
   const fullPath = `${filePath}/${fileName}.mdx`;
 
-  exec(`git checkout -b ${type}/${title}`, async () => {
-    await fs.writeFile(fullPath, template[type](title, today));
+  if (fs.existsSync(fullPath)) {
+    console.error('\x1b[31m%s\x1b[0m', '⛔️ Did you already write this one?');
+    process.exit();
+  }
+
+  exec(`git checkout -b ${type}/${type === 'daily' ? title : fileName}`, async () => {
+    await fsPromises.writeFile(fullPath, template[type](title, today));
     exec(`code ${fullPath}`);
   });
 };
@@ -26,7 +32,7 @@ const huh = async () => {
     {
       type: 'select',
       name: 'postType',
-      message: 'What type of post is this?',
+      message: '🤔 What type of post is this?',
       choices: [
         { title: 'Daily', value: 'daily' },
         { title: 'Writing', value: 'writing' },
@@ -35,7 +41,7 @@ const huh = async () => {
     {
       type: postType => (postType === 'writing' ? 'text' : null),
       name: 'postTitle',
-      message: "What's the title of this post?",
+      message: "🤔 What's the title of this post?",
     },
   ]);
 
