132456d692e614ac7096ec07973bb09655f7fffb
Alan Shortis <alan.shortis@mac.com>
2022-10-07 18:03:15 +0100

    bug/date-format (#515)
    * Fix date formatting
    
    * Missing script

--- src/helpers/date-format.ts
+++ src/helpers/date-format.ts
@@ -1,9 +1,8 @@
 export const dateFormat = (date: string, hasYear?: boolean): string => {
-  const options = {
-    month: 'long',
-    day: 'numeric',
-    year: hasYear ? 'numeric' : undefined,
-  } as const;
+  const d = new Date(date);
+  const year = d.getFullYear();
+  const month = d.toLocaleString('en-GB', { month: 'long' });
+  const day = d.getDate();
 
-  return new Date(date).toLocaleDateString('en-GB', options);
+  return `${day} ${month} ${hasYear ? year : ''}`;
 };
