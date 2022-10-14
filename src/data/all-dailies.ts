7883e046816278ba13adb2782d2c03872d029e2f
Alan <alanshortis@monzo.com>
2022-10-14 15:02:22 +0100

    Add daily post count

--- src/data/all-dailies.ts
+++ src/data/all-dailies.ts
@@ -9,6 +9,8 @@ const allDailyFileNames = fs.readdirSync(DAILY_DIR);
 
 const fileContent = (fileName: string) => matter(fs.readFileSync(path.join(DAILY_DIR, fileName)));
 
+export const dailyCount = fs.readdirSync(DAILY_DIR).length;
+
 export const allDailies = await Promise.all(
   allDailyFileNames.map(async (fileName): Promise<DailyPost> => {
     const { data, content } = fileContent(fileName);
