9d7b18c6864a6ad03471b3e7db9f50e9f3ca55b5
Alan Shortis <alan.shortis@mac.com>
2022-10-23 21:42:56 +0100

    Remove stray photo stuff

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -32,7 +32,6 @@ const StyledList = styled.ul`
 
 const pages: MenuItem[] = [
   { title: 'Writing', path: '/writing' },
-  { title: 'Photography', path: '/photography' },
   { title: 'Daily', path: '/daily' },
 ];
 
