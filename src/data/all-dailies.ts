cef4d9edfd58fddc49e3e33562fee731dacf626e
Alan Shortis <alan.shortis@mac.com>
2022-10-23 21:52:09 +0100

    feature/daily-performance (#545)
    * Create page and menu ite,
    
    * Write a file of daily names
    
    * Remove stray photo stuff
    
    * Fix missing export

--- src/data/all-dailies.ts
+++ src/data/all-dailies.ts
@@ -3,24 +3,24 @@ import path from 'path';
 import matter from 'gray-matter';
 import { serialize } from 'next-mdx-remote/serialize';
 import { DailyPost } from '../types';
+import { dailyDays } from './daily-days';
 
 const EXT = '.mdx';
 const DAILY_DIR = path.join(process.cwd(), 'src/posts/daily');
-const allDailyFileNames = fs.readdirSync(DAILY_DIR);
 
 const fileContent = (fileName: string) => matter(fs.readFileSync(path.join(DAILY_DIR, fileName)));
 
+// Make a new one, because `dailyDays` is frozen.
+export const postDays = [...dailyDays];
+
 // How many posts?
-export const dailyCount = allDailyFileNames.length;
+export const dailyCount = postDays.length;
 
 // How many per page?
 export const PER_PAGE = 10;
 
 // How many pages?
 export const pageCount = Math.ceil(dailyCount / PER_PAGE);
-
-// Posts are in files, named by the day. Strip the file extension and make a number.
-export const postDays = allDailyFileNames.map(fileName => Number(fileName.replace(EXT, '')));
 
 // An array of posts in a range, with parsed MDX content.
 // Using the offset and count args, can get a single post.
