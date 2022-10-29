14da371ee587935ebe732b527947a9dd51a718f6
Alan Shortis <alan.shortis@mac.com>
2022-10-29 18:56:16 +0100

    Remove config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-/* eslint-disable no-param-reassign */
 module.exports = {
-  webpack: config => {
-    config.experiments = { ...config.experiments, ...{ topLevelAwait: true } };
-
-    return config;
-  },
   redirects: () => {
     return [
       {
