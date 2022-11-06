06b8d4c54e3d49e3f1934ca259bb68b9e17e404f
Alan Shortis <alan.shortis@mac.com>
2022-11-06 17:50:57 +0000

    feature/ordinal (#568)

--- src/components/PostDate.tsx
+++ src/components/PostDate.tsx
@@ -1,11 +1,18 @@
 import type { FC } from 'react';
 import dynamic from 'next/dynamic';
+import styled from 'styled-components';
 import { dateFormat } from '../helpers';
 import { Label } from '.';
 
 const ShareButton = dynamic(() => import('./ShareButton'), {
   ssr: process.env.NODE_ENV === 'production',
 });
+
+const Time = styled.time`
+  sup {
+    font-size: 0.5em;
+  }
+`;
 
 interface Props {
   date: string;
@@ -17,7 +24,7 @@ interface Props {
 export const PostDate: FC<Props> = ({ date, prefix, hasShare, hasYear }) => (
   <Label>
     {prefix && <span>{prefix} &middot; </span>}
-    <time dateTime={date}>{dateFormat(date, hasYear)}</time>
+    <Time dateTime={date} dangerouslySetInnerHTML={{ __html: dateFormat(date, hasYear) }} />
     {hasShare && <ShareButton />}
   </Label>
 );
