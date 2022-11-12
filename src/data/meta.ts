134c62ac6ea9593b549d85ee67afd179544dced0
Alan Shortis <alan.shortis@mac.com>
2022-11-12 18:56:55 +0000

    New about page

--- src/data/meta.ts
+++ src/data/meta.ts
@@ -8,6 +8,13 @@ export const meta = Object.freeze({
   twitter: '@alanshortis',
   url: 'https://short.is',
   year: new Date().getFullYear(),
+  social: [
+    { name: 'CodePen', url: 'https://codepen.io/alanshortis' },
+    { name: 'GitHub', url: 'https://github.com/alanshortis' },
+    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/alan-shortis-575a1b35/' },
+    { name: 'Strava', url: 'https://www.strava.com/athletes/138800' },
+    { name: 'Twitter', url: 'https://twitter.com/alanshortis' },
+  ],
 });
 
 export const MetaContext = createContext(meta);
