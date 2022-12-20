cab20ee3f195e25e021897f9d08116c62111f0bb
Alan Shortis <alan.shortis@mac.com>
2022-12-20 19:41:02 +0000

    daily/126 (#635)
    * Update selected
    
    * Daily post

--- src/data/selected-dailies.ts
+++ src/data/selected-dailies.ts
@@ -1,3 +1,3 @@
 import { dailyDays } from './daily-days';
 
-export const selectedDailies: Readonly<number[]> = Object.freeze([dailyDays[0], 108, 88, 74, 16]);
+export const selectedDailies: Readonly<number[]> = Object.freeze([dailyDays[0], 108, 88, 39, 4]);
