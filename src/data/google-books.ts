8ae4f5dfca699fc53fb901adf7ad11ca212a0b64
Alan Shortis <alan.shortis@mac.com>
2023-01-06 15:24:42 +0000

    feature/offline-mode

--- src/data/google-books.ts
+++ src/data/google-books.ts
@@ -1,6 +1,12 @@
 import type { Book } from '../types';
 
+const isOffline = process.env.OFFLINE === 'offline';
+
+const mock: Book = { title: 'Some book', author: 'Some writer', url: '/' };
+
 export const googleBooks = async (): Promise<Book> => {
+  if (isOffline) return mock;
+
   const res = await fetch(
     `https://www.googleapis.com/books/v1/users/${process.env.GOOGLE_BOOKS_USER}/bookshelves/3/volumes?key=${process.env.GOOGLE_BOOKS_KEY}`
   );
