ac1bd2beaefda5de3b689129d9d4e3317439354a
Alan Shortis <alan.shortis@mac.com>
2023-02-05 23:36:08 +0000

    We using CSS modules then?

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import { StrictMode } from 'react';
 import { MetaProvider } from '@/context';
+import '@/styles/global.scss';
 
 const Shortis = ({ Component, pageProps }: AppProps) => (
   <StrictMode>
