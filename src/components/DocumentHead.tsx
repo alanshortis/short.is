348b2b4d2e34717d9445cf9bcc015e6aa586ba41
Alan Shortis <alan.shortis@mac.com>
2023-02-26 22:02:55 +0000

    Icons

--- src/components/DocumentHead.tsx
+++ src/components/DocumentHead.tsx
@@ -1,4 +1,4 @@
-import NextHead from 'next/head';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 interface Props {
@@ -12,22 +12,22 @@ export const DocumentHead = ({ title, intro }: Props) => {
   const description = intro || 'Alan Shortis is a web developer';
 
   return (
-    <NextHead>
+    <Head>
       <title>{pageTitle}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content={description} />
       <meta property="og:title" content={pageTitle} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={'https://short.is' + router.asPath} />
-      {/* <meta property="og:image" content="https://short.is/icons/og.png" /> */}
+      <meta property="og:image" content="https://short.is/og.png" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content="@alanshortis" />
       <meta name="twitter:title" content={pageTitle} />
       <meta name="twitter:description" content={description} />
-      {/* <meta name="twitter:image" content="https://short.is/icons/og.png" /> */}
-      {/* <link rel="icon" type="image/png" href="/icons/favicon.png" /> */}
-      <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
+      <meta name="twitter:image" content="https://short.is/og.png" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <link rel="alternate" type="application/rss+xml" title="RSS Feed for short.is" href="/daily.xml" />
-    </NextHead>
+    </Head>
   );
 };
