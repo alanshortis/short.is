37b2b8b66e0b2cd4ffe32912254711055d7a5507
Alan Shortis <alan.shortis@mac.com>
2023-02-26 18:20:12 +0000

    Remove full fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,9 @@ import type { NextPage } from 'next';
 import { PageLayout } from '@/layouts';
 
 const Home: NextPage = () => (
-  // <PageLayout>
-  //   <h1 className="hidden">Home</h1>
-  <>
-    <p>abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890</p>
-  </>
-  // </PageLayout>
+  <PageLayout>
+    <h1 className="hidden">Home</h1>
+  </PageLayout>
 );
 
 export default Home;
