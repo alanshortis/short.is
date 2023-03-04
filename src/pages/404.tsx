93d6aa709d97ced43d38ee76622af8015276025a
Alan Shortis <alan.shortis@mac.com>
2023-03-04 15:11:29 +0000

    404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,21 @@
 import type { NextPage } from 'next';
-import { PageLayout } from '@/layouts';
+import { Page } from '@/layouts';
+import layoutStyles from '@/layouts/Page/Page.module.scss';
+import contentStyles from '@/components/Markdown/Markdown.module.scss';
 
 const NotFound: NextPage = () => (
-  <PageLayout title="NotFound">
-    <h1>NotFound</h1>
-  </PageLayout>
+  <Page title="404">
+    <div className={layoutStyles.aside}>
+      <h2>404</h2>
+    </div>
+    <article className={`${layoutStyles.mainContent} ${contentStyles.markdown}`}>
+      <p className={contentStyles.large}>Whatever you&rsquo;re looking for is not here.</p>
+      <p>
+        short.is used to be a domain shortening service, so there is a chance you followed a link with the
+        hope of seeing something cool. This probably isn&rsquo;t it.
+      </p>
+    </article>
+  </Page>
 );
 
 export default NotFound;
