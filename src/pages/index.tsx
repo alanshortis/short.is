be4a35a551f4b6f5462e3d10a068839b9039d03c
Alan Shortis <alan.shortis@mac.com>
2023-03-05 19:14:38 +0000

    No footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from 'next';
 import { Full } from '@/layouts';
+import styles from '@/layouts/Full/Full.module.scss';
 
 const Home: NextPage = () => (
   <Full>
     <h1 className="hidden">Home</h1>
+    <div className={styles.grid}>
+      <div>1</div>
+      <div>
+        <div className={styles.orb}></div>
+      </div>
+      <div>3</div>
+      <div>4</div>
+    </div>
   </Full>
 );
 
