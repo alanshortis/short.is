f85971d4987b75a734a1a831a00331d5710d969d
Alan Shortis <alan.shortis@mac.com>
2023-03-28 20:17:34 +0100

    feature/classnames (#747)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import c from 'classnames';
 import { Page } from '@/layouts';
 import layoutStyles from '@/layouts/Page/Page.module.scss';
 import contentStyles from '@/components/Markdown/Markdown.module.scss';
@@ -6,7 +7,7 @@ import contentStyles from '@/components/Markdown/Markdown.module.scss';
 const NotFound: NextPage = () => (
   <Page title="404">
     <h2 className={layoutStyles.title}>404</h2>
-    <article className={`${layoutStyles.content} ${contentStyles.markdown}`}>
+    <article className={c(layoutStyles.content, contentStyles.markdown)}>
       <p className={contentStyles.large}>Whatever you&rsquo;re looking for is not here.</p>
       <p>
         short.is used to be a domain shortening service, so there is a chance you followed a link with the
