4b2dec1109e52f36afe0362c4b6e6f54f1bb9ef1
Alan Shortis <alan.shortis@mac.com>
2023-09-25 20:55:34 +0100

    Layout

--- src/layouts/Full/index.tsx
+++ src/layouts/Full/index.tsx
@@ -3,13 +3,14 @@ import { DocumentHead, Header } from '@/components';
 import styles from './Full.module.scss';
 
 interface Props {
+  title: string;
   children: React.ReactNode;
   className?: string;
 }
 
-export const Full = ({ children, className }: Props) => (
+export const Full = ({ children, className, title }: Props) => (
   <>
-    <DocumentHead />
+    <DocumentHead title={title} />
     <Header />
     <main id="top" className={c(styles.main, className)}>
       {children}
--- src/pages/photography/index.tsx
+++ src/pages/photography/index.tsx
@@ -1,16 +1,16 @@
 import type { NextPage } from 'next';
 import cx from 'classnames';
-import { Page } from '@/layouts';
+import { Full } from '@/layouts';
 import layoutStyles from '@/layouts/Page/Page.module.scss';
 import contentStyles from '@/components/Markdown/Markdown.module.scss';
 
 const Photography: NextPage = () => (
-  <Page title="Photography">
+  <Full title="Photography">
     <h2 className={layoutStyles.title}>Photography</h2>
     <article className={cx(layoutStyles.content, contentStyles.markdown)}>
       <p>Blah blah</p>
     </article>
-  </Page>
+  </Full>
 );
 
 export default Photography;
