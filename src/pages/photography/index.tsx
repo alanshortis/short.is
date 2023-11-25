d6d534b95b80889aef6f073b59e8b98f8c982b3b
Alan Shortis <alan.shortis@mac.com>
2023-11-25 20:50:50 +0000

    Photography layout progress

--- src/pages/photography/index.tsx
+++ src/pages/photography/index.tsx
@@ -1,12 +1,37 @@
 import type { NextPage } from 'next';
 import { Full } from '@/layouts';
+import { Footer } from '@/components';
+import styles from './Photography.module.scss';
 
 const Photography: NextPage = () => (
-  <Full title="Photography">
-    <article>
-      <p>Blah blah</p>
-    </article>
-  </Full>
+  <>
+    <Full title="Photography">
+      <article>
+        <div className={styles.masonry}>
+          <div style={{ aspectRatio: '3 / 2' }}></div>
+          <div style={{ aspectRatio: '7 / 6' }}></div>
+          <div style={{ aspectRatio: '7 / 6' }}></div>
+          <div style={{ aspectRatio: '6 / 6' }}></div>
+          <div style={{ aspectRatio: '6 / 7' }}></div>
+          <div style={{ aspectRatio: '7 / 6' }}></div>
+          <div style={{ aspectRatio: '2 / 3' }}></div>
+          <div style={{ aspectRatio: '6 / 6' }}></div>
+          <div style={{ aspectRatio: '7 / 6' }}></div>
+          <div style={{ aspectRatio: '3 / 2' }}></div>
+          <div style={{ aspectRatio: '6 / 6' }}></div>
+          <div style={{ aspectRatio: '3 / 2' }}></div>
+          <div style={{ aspectRatio: '7 / 6' }}></div>
+          <div style={{ aspectRatio: '7 / 6' }}></div>
+          <div style={{ aspectRatio: '6 / 6' }}></div>
+          <div style={{ aspectRatio: '6 / 7' }}></div>
+          <div style={{ aspectRatio: '7 / 6' }}></div>
+          <div style={{ aspectRatio: '3 / 2' }}></div>
+          <div style={{ aspectRatio: '6 / 6' }}></div>
+        </div>
+      </article>
+    </Full>
+    <Footer />
+  </>
 );
 
 export default Photography;
