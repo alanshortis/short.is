7cbd07f9475a1ff1951bf7e7c8495b4bb4b92010
Alan Shortis <alan.shortis@mac.com>
2024-03-30 13:42:24 +0000

    Photo data

---
+++ src/data/photos/kit.ts
@@ -0,0 +1,32 @@
+export enum Camera {
+  BRONICA = 'Bronica EC-TL',
+  FUJI_67 = 'Fuji GW670II',
+  HOLGA_SF = 'Holga 120SF',
+  HOLGA_TLR = 'Holga 120TLR',
+  LEICA_M6 = 'Leica M6',
+  LEICA_M_262 = 'Leica M (Typ 262)',
+}
+
+export enum Lens {
+  BRONICA = 'Zenzanon 80mm f/2.8',
+  LEICA_50 = 'Leica Summicron 50mm f/2',
+  VOIGHTLANDER_15 = 'Voightlander Heliar 15mm f/4.5',
+}
+
+export enum Film {
+  DELTA_3200 = 'Ilford Delta 3200',
+  DELTA_400 = 'Ilford Delta 400',
+  EKTAR_100 = 'Kodak Ektar 100',
+  FUJI_400H = 'Fuji Pro 400H',
+  ACROS_100 = 'Fuji Acros 100',
+  PORTRA_400 = 'Kodak Portra 400',
+  TMAX_3200 = 'Kodak T-Max 3200',
+}
+
+export enum Ratio {
+  SIXSEVEN_L = '7 / 6',
+  SIXSEVEN_P = '6 / 7',
+  SIXSIX = '1 / 1',
+  THIRTYFIVE_L = '3 / 2',
+  THIRTYFIVE_P = '2 / 3',
+}
