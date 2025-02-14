b1babf7c647528b6b9edc88a7b9e47df63319714
Alan Shortis <alan.shortis@mac.com>
2025-02-14 20:50:48 +0000

    feature/color (#1006)

--- src/components/Markdown/renderers/CodeBlock/index.tsx
+++ src/components/Markdown/renderers/CodeBlock/index.tsx
@@ -15,12 +15,12 @@ export const CodeBlock: FC<Props> = ({ children }) => {
   return (
     <div className={styles.codeBlock}>
       <div className={styles.blockHeader}>
-        <div className={styles.dumbDots}>
-          <span />
-          <span />
-          <span />
-        </div>
-        <button type="button" onClick={handleCopy}>
+        <svg width="32" height="8" viewBox="0 0 32 8" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="4" cy="4" r="4" />
+          <circle cx="16" cy="4" r="4" />
+          <circle cx="28" cy="4" r="4" />
+        </svg>
+        <button type="button" title="Copy this code snippet" onClick={handleCopy}>
           Copy
         </button>
       </div>
