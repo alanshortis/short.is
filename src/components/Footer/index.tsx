f00490d46ffd68eee5a81ed62cb4e22a5f54cb1d
Alan Shortis <alan.shortis@mac.com>
2025-02-14 00:30:34 +0000

    Switch up header and footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,10 @@
 import { type FC } from 'react';
 import styles from './Footer.module.scss';
-import { SchemeToggle } from '@/components';
+import { SchemeToggle, Logo } from '@/components';
 
 export const Footer: FC = () => (
   <footer className={styles.footer}>
-    <p>Alan Shortis</p>
+    <Logo />
     <SchemeToggle />
   </footer>
 );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,12 @@
 import { type FC } from 'react';
 import Link from 'next/link';
-import { Clock, Logo } from '@/components';
+import { Clock } from '@/components';
 import styles from './Header.module.scss';
 
 export const Header: FC = () => (
   <header className={styles.header}>
     <span className={styles.hidden}>Alan Shortis</span>
-    <Link href="/">
-      <Logo />
-    </Link>
+    <Link href="/">Alan Shortis</Link>
     <Clock />
   </header>
 );
