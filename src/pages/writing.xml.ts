ab8ea520b1faebd6373d6548caf167bc03cff3ed
Alan Shortis <alan.shortis@mac.com>
2025-02-19 11:12:14 +0000

    RSS and reset bugs

--- src/pages/writing.xml.ts
+++ src/pages/writing.xml.ts
@@ -1,15 +1,12 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import type { APIContext } from 'astro';
-import type { Post } from '../types';
-
-const PATH = './**/*.md';
 
 export async function GET(context: APIContext) {
   return rss({
     title: 'Alan Shortis',
     description: 'Recent writing from short.is',
     site: context.site || '',
-    items: await pagesGlobToRssItems(import.meta.glob<Post>(PATH)),
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: '<language>en-GB</language>',
   });
 }
