8ff2c965be3665d227b7cf9a12ebba5cb2f05e5b
Alan Shortis <alan.shortis@mac.com>
2025-02-20 00:55:46 +0000

    Render as markdown with components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://short.is',
+
   markdown: {
     syntaxHighlight: false,
   },
+
+  integrations: [mdx(), sitemap()],
 });
---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as Clock } from './Clock.astro';
+export { default as CodeBlock } from './CodeBlock.astro';
+export { default as Footer } from './Footer.astro';
+export { default as Header } from './Header.astro';
+export { default as Logo } from './Logo.astro';
+export { default as PostDate } from './PostDate.astro';
+export { default as SchemeToggle } from './SchemeToggle.astro';
+export { default as TableOfContents } from './TableOfContents.astro';
