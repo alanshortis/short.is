09ea3f67b3d550d8aec731ed8eca7a256d8fab3e
Alan Shortis <alan.shortis@mac.com>
2025-02-20 21:51:55 +0000

    Sort posts, fix rss

--- src/pages/writing.xml.ts
+++ src/pages/writing.xml.ts
@@ -1,12 +1,20 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import type { APIContext } from 'astro';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context: APIContext) {
+  const blog = await getCollection('posts');
   return rss({
     title: 'Alan Shortis',
     description: 'Recent writing from short.is',
     site: context.site || '',
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: '<language>en-GB</language>',
+    items: blog
+      .map(post => ({
+        title: post.data.title,
+        pubDate: new Date(post.data.pubDate),
+        description: post.data.description,
+        link: `/writing/${post.slug}/`,
+      }))
+      .sort((a, b) => b.pubDate.getTime() - a.pubDate.getTime()),
   });
 }
