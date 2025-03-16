b4e4f7f6ec50384451489aef1d78c87176717316
Alan Shortis <alan.shortis@mac.com>
2025-03-16 12:58:33 +0000

    Set up cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 import mdx from '@astrojs/mdx';
 
@@ -7,10 +8,10 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://short.is',
-
+  output: 'server',
+  adapter: cloudflare(),
+  integrations: [mdx(), sitemap()],
   markdown: {
     syntaxHighlight: false,
   },
-
-  integrations: [mdx(), sitemap()],
 });
