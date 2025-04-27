be331a24819827bbd4366d03535e405663bd4671
Alan Shortis <alan.shortis@mac.com>
2025-04-27 22:26:17 +0100

    Tidy deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://short.is',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx()],
   markdown: {
     syntaxHighlight: false,
   },
