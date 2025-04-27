3f81de36ff5d35c79b81780f5e664d757045efeb
Alan Shortis <alan.shortis@mac.com>
2025-04-27 22:42:28 +0100

    feature/simplify (#1046)
    * Tidy deps
    
    * Better circle shit
    
    * Reading as a collection
    
    * Tidy

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,5 @@
 import { defineCollection, z } from 'astro:content';
+import { file } from 'astro/loaders';
 
 const posts = defineCollection({
   type: 'content',
@@ -12,7 +13,17 @@ const posts = defineCollection({
     }),
 });
 
-// The `posts` key matches the folder name in `src/content/posts`
+const reading = defineCollection({
+  loader: file('src/content/reading.json'),
+  schema: () =>
+    z.object({
+      url: z.string().url(),
+      title: z.string(),
+      author: z.string(),
+    }),
+});
+
 export const collections = {
   posts,
+  reading,
 };
