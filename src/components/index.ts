2f3ccd7e54466114fba340595b8651d06fa5232e
Alan Shortis <alan.shortis@mac.com>
2025-05-04 22:05:29 +0100

    Remove the garbage

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { default as AgeWarning } from './AgeWarning.astro';
 export { default as Circles } from './Circles.astro';
 export { default as Clock } from './Clock.astro';
-export { default as CodeBlock } from './CodeBlock.astro';
 export { default as Concerns } from './Concerns.astro';
 export { default as ExampleEmbed } from './ExampleEmbed.astro';
 export { default as Footer } from './Footer.astro';
@@ -10,4 +9,3 @@ export { default as Logo } from './Logo.astro';
 export { default as PostDate } from './PostDate.astro';
 export { default as ReadingList } from './ReadingList.astro';
 export { default as SchemeToggle } from './SchemeToggle.astro';
-export { default as Share } from './Share.astro';
