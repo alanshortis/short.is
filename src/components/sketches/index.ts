31851a6ccdc16d486d108ebf6c85704fd0d6fbde
Alan Shortis <alan.shortis@mac.com>
2025-05-12 22:26:29 +0100

    Create and render sketches

---
+++ src/components/sketches/index.ts
@@ -0,0 +1,6 @@
+export { default as Anaglyph } from './Anaglyph.astro';
+export { default as Circles } from './Circles.astro';
+export { default as Cube } from './Cube.astro';
+export { default as Glow } from './Glow.astro';
+export { default as Perspective } from './Perspective.astro';
+export { default as Shaft } from './Shaft.astro';
