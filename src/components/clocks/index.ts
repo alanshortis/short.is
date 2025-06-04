136fe624ec09ff407938bfbe79dbb82c8a20b49f
Alan Shortis <alan.shortis@mac.com>
2025-06-04 21:53:38 +0100

    Progress bar clock

---
+++ src/components/clocks/index.ts
@@ -0,0 +1,3 @@
+export { default as DecimalClock } from './DecimalClock.astro';
+export { default as DecimalProgress } from './DecimalProgress.astro';
+export { default as RegularClock } from './RegularClock.astro';
