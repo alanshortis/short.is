efff09f9a995b38332e46f5ce16bd17eaca7aabd
Alan Shortis <alan.shortis@mac.com>
2025-06-14 13:39:39 +0100

    Analogue clock progress

--- src/components/clocks/index.ts
+++ src/components/clocks/index.ts
@@ -1,3 +1,4 @@
+export { default as DecimalAnalog } from './DecimalAnalog.astro';
 export { default as DecimalClock } from './DecimalClock.astro';
 export { default as DecimalProgress } from './DecimalProgress.astro';
 export { default as RegularClock } from './RegularClock.astro';
