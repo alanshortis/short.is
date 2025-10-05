b71f98ddb60dc813b43658797915382d989a7dd4
Alan Shortis <alan.shortis@mac.com>
2025-10-05 20:09:13 +0100

    feature/clock-workers (#1074)
    * Add package for worker support
    
    * Decimal worker
    
    * Standard decimal clock
    
    * Update decimal clocks
    
    * Update consistency
    
    * Clean

---
+++ src/workers/decimal-clock.ts
@@ -0,0 +1,30 @@
+import type { DecimalTime } from '../shared-types';
+
+const TOTAL_SECONDS_IN_DAY = 24 * 60 * 60;
+const TOTAL_METRIC_SECONDS_IN_DAY = 10 * 100 * 100;
+const DECIMAL_SECOND_MS = 864;
+
+function getDecimalTime(): DecimalTime {
+  const now = new Date();
+  const secondsSinceMidnight =
+    now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds() + now.getMilliseconds() / 1000;
+
+  const fractionOfDay = secondsSinceMidnight / TOTAL_SECONDS_IN_DAY;
+  const metricSecondsElapsed = fractionOfDay * TOTAL_METRIC_SECONDS_IN_DAY;
+
+  const hours = Math.floor(metricSecondsElapsed / 10_000);
+  const minutes = Math.floor((metricSecondsElapsed % 10_000) / 100);
+  const seconds = Math.floor(metricSecondsElapsed % 100);
+
+  return {
+    hours,
+    minutes,
+    seconds,
+  };
+}
+
+postMessage(getDecimalTime());
+
+setInterval(() => {
+  postMessage(getDecimalTime());
+}, DECIMAL_SECOND_MS);
