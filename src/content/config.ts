95dff2382c49c4798f5ebfd619a7988673fc3a03
Alan Shortis <alan.shortis@mac.com>
2026-02-28 11:55:59 +0000

    Photo collection schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,24 +1,26 @@
 import { defineCollection, z } from 'astro:content';
 import { file } from 'astro/loaders';
 
-const cameras = z.enum(['Leica M6', 'Leica M10-P']);
+const cameras = z.enum(['Leica M6', 'Leica M10 Monochrom', 'Leica M10-P', 'Leica M-D', 'Fujifilm GW670ii']);
 
 const lenses = z.enum(['Leica Summicron-M 35mm f/2.0 ASPH', 'Leica Summicron 50mm f/2.0']);
 
 const film = z.enum(['Kodak Portra 400', 'Ilford Delta 3200']);
 
-const photographySchema = z.object({
-  id: z.number(), // Year
-  photographs: z.array(
-    z.object({
-      name: z.string(),
-      location: z.string(),
-      camera: cameras,
-      lens: lenses.optional(),
-      film: film.optional(),
-      altText: z.string(),
-    })
-  ),
+const photographySchema = z.array(
+  z.object({
+    name: z.string(),
+    location: z.string(),
+    camera: cameras,
+    lens: lenses.optional(),
+    film: film.optional(),
+    altText: z.string(),
+  })
+);
+
+const photography = defineCollection({
+  loader: file('src/content/photography.json'),
+  schema: () => photographySchema,
 });
 
 const posts = defineCollection({
@@ -42,11 +44,6 @@ const reading = defineCollection({
       title: z.string(),
       author: z.string(),
     }),
-});
-
-const photography = defineCollection({
-  loader: file('src/content/photography.json'),
-  schema: () => photographySchema,
 });
 
 export const collections = {
