c5935cbb9514ad4b726f81cbb12cd1ba8067d36b
Alan Shortis <alan.shortis@mac.com>
2026-05-22 15:36:48 +0100

    Add new illustration, update location

--- src/components/content-icons/index.ts
+++ src/components/content-icons/index.ts
@@ -4,6 +4,7 @@ import DarkMode from './DarkMode.astro';
 import Ethics from './Ethics.astro';
 import Favicon from './Favicon.astro';
 import Figma from './Figma.astro';
+import Holga from './Holga.astro';
 import Hopper from './Hopper.astro';
 import Imposter from './Imposter.astro';
 import Internet from './Internet.astro';
@@ -16,21 +17,22 @@ import Sprites from './Sprites.astro';
 import Website from './Website.astro';
 import Zindex from './Zindex.astro';
 export const icons = {
-  'ai': Ai,
+  ai: Ai,
   'css-env': CssEnv,
   'dark-mode': DarkMode,
-  'ethics': Ethics,
-  'favicon': Favicon,
-  'figma': Figma,
-  'hopper': Hopper,
-  'imposter': Imposter,
-  'internet': Internet,
+  ethics: Ethics,
+  favicon: Favicon,
+  figma: Figma,
+  holga: Holga,
+  hopper: Hopper,
+  imposter: Imposter,
+  internet: Internet,
   'new-york': NewYork,
-  'observer': Observer,
-  'responsive': Responsive,
-  'scanning': Scanning,
-  'share': Share,
-  'sprites': Sprites,
-  'website': Website,
+  observer: Observer,
+  responsive: Responsive,
+  scanning: Scanning,
+  share: Share,
+  sprites: Sprites,
+  website: Website,
   'z-index': Zindex,
 };
---
+++ src/components/illustrations/index.ts
@@ -0,0 +1,3 @@
+export { default as EsbIllustration } from './EsbIllustration.astro';
+export { default as CameraIllustration } from './CameraIllustration.astro';
+export { default as HolgaIllustration } from './HolgaIllustration.astro';
