79fd5ffc620efa09c3cc78ca438e0966339f2f46
Alan Shortis <alan.shortis@mac.com>
2026-05-29 17:15:14 +0100

    feature/astro-upgrade (#1132)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@
 import mdx from '@astrojs/mdx';
 import worker from '@astropub/worker';
 import { defineConfig } from 'astro/config';
+import { satteri } from '@astrojs/markdown-satteri';
 
 export default defineConfig({
   site: 'https://short.is',
   integrations: [mdx(), worker()],
   markdown: {
     syntaxHighlight: false,
+    processor: satteri({
+      features: { directive: true },
+    }),
   },
   redirects: {
     '/writing/ai-ambivalence/': {
