59a90a761c566fb07f265aebd54f34c8dfcf8c06
Alan Shortis <alan.shortis@mac.com>
2026-06-18 23:20:06 +0100

    Both clock modes

--- src/components/clocks/index.ts
+++ src/components/clocks/index.ts
@@ -1,3 +1,3 @@
-export { default as DecimalAnalog } from './DecimalAnalog.astro';
+export { default as AnalogClock } from './AnalogClock.astro';
 export { default as DigitalClock } from './DigitalClock.astro';
 export { default as DecimalProgress } from './DecimalProgress.astro';
